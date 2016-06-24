5a7bd4de3fe5da63e51ae727e6c4e6e15ed8eb66
Vladimir Petrigo <taenaru@gmail.com>
2016-06-25 01:36:01 +0300

    implement the solution for the topological sort problem

--- alg-graph/week2/top-sort/tsort.cpp
+++ alg-graph/week2/top-sort/tsort.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <algorithm>
 #include <functional>
+#include <iterator>
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -14,6 +15,8 @@ enum class VerticeStatus {
   visited
 };
 
+
+// fill adjacent list with input values
 void fill_vector(const int m, GraphRepr& graph) {
   for (std::size_t i = 0; i < m; ++i) {
     int u, v;
@@ -23,40 +26,45 @@ void fill_vector(const int m, GraphRepr& graph) {
   }
 }
 
-void explore(const GraphRepr &graph, int vertice, std::vector<bool> visited,
-             std::vector<int> &pre, std::vector<int> &post) {
-  VerticeStack process_stack{{vertice, graph[vertice].cbegin()}};
-  int pre_time = 0;
-  int post_time = 0;
+void explore(const GraphRepr &graph, int vertice, std::vector<VerticeStatus>& visited,
+             std::vector<int> &pre, std::vector<int> &post, int& time) {
+  VerticeStack process_stack;
 
-  if (pre.empty() || post.empty()) {
-    pre_time = 1;
-    post_time = pre_time;
-  }
-  else {
-    pre_time = pre.back();
-    post_time = post.back();
-  }
+  process_stack.push(std::make_pair(vertice, graph[vertice].begin()));
+  pre[vertice] = time++;
 
   while (!process_stack.empty()) {
-    auto processing_vertice = process_stack.top();
-
-    pre[processing_vertice.first] = pre_time++;
-    if (!visited[processing_vertice]) {
-      for (auto e : graph[processing_vertice]) {
-        process_stack.emplace(e);
-      }
+    auto& processing_vertice = process_stack.top();
+    visited[processing_vertice.first] = VerticeStatus::processing;
+    // if the current vertice's adjacent list is empty or
+    // we reach the end of that list mark that vertice as visited and exit
+    if (graph[processing_vertice.first].empty() ||
+        processing_vertice.second == std::end(graph[processing_vertice.first])) {
+      visited[processing_vertice.first] = VerticeStatus::visited;
+      post[processing_vertice.first] = time++;
+      process_stack.pop();
     }
+    else {
+      // look up for unvisited verticies
+      // we might find already visited verticies
+      if (visited[*processing_vertice.second] ==
+          VerticeStatus::unvisited) {
+        process_stack.emplace(std::make_pair(
+            *processing_vertice.second,std::begin(graph[*processing_vertice.second])));
+        pre[*processing_vertice.second] = time++;
+      }
 
-    visited[vertice] = true;
+      processing_vertice.second = std::next(processing_vertice.second);
+    }
   }
 }
 void dfs(const GraphRepr& graph, std::vector<int>& pre, std::vector<int>& post) {
-  std::vector<bool> visited(graph.size(), false);
+  std::vector<VerticeStatus> visited(graph.size(), VerticeStatus::unvisited);
+  int time = 1;
 
   for (std::size_t i = 0; i < graph.size(); ++i) {
-    if (!visited[i]) {
-      explore(graph, i, visited, pre, post);
+    if (visited[i] != VerticeStatus::visited) {
+      explore(graph, i, visited, pre, post, time);
     }
   }
 }
@@ -74,19 +82,24 @@ std::vector<std::pair<int, int>> get_vertice_index(const std::vector<int>& posto
   std::vector<std::pair<int, int>> out(postorder.size());
   std::size_t vertice_index = 1;
 
-  std::transform(postorder.cbegin(), postorder.cend(), out.begin(), [](const int& posttime) {
+  std::transform(postorder.cbegin(), postorder.cend(), out.begin(), [&vertice_index](const int& posttime) {
     return std::make_pair(posttime, vertice_index++);
   });
+
+  return out;
 }
 
 int main() {
   int n, m;
   std::cin >> n >> m;
   GraphRepr graph(n);
+
+  fill_vector(m, graph);
+
   auto post_o = topological_sort(graph);
   auto ans = get_vertice_index(post_o);
 
-  std::sort(ans.cbegin(), ans.cend(), std::greater<std::pair<int, int>> ());
+  std::sort(ans.begin(), ans.end(), std::greater<std::pair<int, int>> ());
   for (const auto& elem : ans) {
     std::cout << elem.second << ' ';
   }
