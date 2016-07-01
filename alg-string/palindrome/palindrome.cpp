fc518a1124435c9837b8f53a538e9bbf2c752d62
Vladimir Petrigo <taenaru@gmail.com>
2016-07-02 02:03:41 +0300

    add solution for the palindrome index problem

---
+++ alg-string/palindrome/palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+int can_be_palindrome(const std::string &s);
+
+int main() {
+  std::string palindrome_candidate;
+
+  std::cin >> palindrome_candidate;
+  std::cout << can_be_palindrome(palindrome_candidate) << std::endl;
+
+  return 0;
+}
+
+int can_be_palindrome(const std::string &s) {
+  auto mid = std::next(std::cbegin(s), s.size() / 2);
+  auto diff_it = std::mismatch(std::cbegin(s), mid, std::crbegin(s)).first;
+  auto diff_index = std::distance(std::cbegin(s), diff_it);
+
+  if (std::equal(std::cbegin(s), diff_it, std::crbegin(s)) &&
+      std::equal(std::next(diff_it), mid, std::next(std::crbegin(s), diff_index))) {
+    return diff_index;
+  }
+  else {
+    return s.size() - diff_index - 1;
+  }
+}
+
