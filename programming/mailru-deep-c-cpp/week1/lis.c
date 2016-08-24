92a29bac0fd3c732918789d4a1140e9ee5e70148
Vladimir Petrigo <taenaru@gmail.com>
2016-08-25 01:22:33 +0300

    solution for the LIS problem

--- programming/mailru-deep-c-cpp/week1/lis.c
+++ programming/mailru-deep-c-cpp/week1/lis.c
@@ -19,7 +19,9 @@ double *read_sequence(int64_t seq_size);
 void print_sequence(const double *seq, size_t size);
 uint32_t lis(const double *seq, size_t size, size_t *index);
 void init_dist(uint32_t *dist, size_t size);
-uint32_t find_max(const uint32_t *seq, size_t size);
+uint32_t find_max(const uint32_t *seq, size_t size, size_t *index);
+void find_first_in_seq(const double *seq, const uint32_t *dist, 
+                         uint32_t max_length, size_t *index);
 
 int main() {
   int64_t N = 0;
@@ -29,13 +31,18 @@ int main() {
       (sequence = read_sequence(N)) != NULL) {
     test_seq_size(N);
     test_seq_ptr(sequence);
-    print_sequence(sequence, N);
+    // print_sequence(sequence, N);
     size_t index;
     size_t length = lis(sequence, N, &index);
 
     if (length > 1) {
-      printf("%" PRIuMAX " %" PRIuMAX, length, index);
+      printf("%" PRIuMAX " %" PRIuMAX, length, index + 1);
     }
+    else {
+      puts("0");
+    }
+
+    free(sequence);
   }
   else {
     puts("[error]");
@@ -124,25 +131,18 @@ uint32_t lis(const double *seq, size_t size, size_t *index) {
   init_dist(dist, size);
 
   for (size_t i = 1; i < size; ++i) {
-    for (size_t j = 0; j < i; ++j) {
-      if (seq[j] <= seq[i]) {
-        dist[i] = fmax(dist[i], dist[j] + 1);
-      }
+    if (seq[i - 1] <= seq[i]) {
+      dist[i] = dist[i - 1] + 1;
     }
   }
 
-  printf("Dist: ");
-  for (size_t i = 0; i < size; ++i) {
-    printf("%" PRIu32, dist[i]);
-    if (i != size - 1) {
-      putchar(' ');
-    }
+  uint32_t max_length = find_max(dist, size, index);
+
+  if (max_length > 1) {
+    find_first_in_seq(seq, dist, max_length, index);
   }
-  puts("");
-
-  uint32_t max_length = find_max(dist, size);
-  *index = 1;
-
+  
+  // clean up helper dist array
   free(dist);
 
   return max_length;
@@ -154,15 +154,45 @@ void init_dist(uint32_t *dist, size_t size) {
   }
 }
 
-uint32_t find_max(const uint32_t *seq, size_t size) {
+uint32_t find_max(const uint32_t *seq, size_t size, size_t *index) {
   assert(size > 0);
   uint32_t max_length = seq[0];
+  *index = 0;
 
   for (size_t i = 1; i < size; ++i) {
     if (max_length < seq[i]) {
       max_length = seq[i];
+      *index = i;
     }
   }
 
   return max_length;
+}
+
+void find_first_in_seq(const double *seq, const uint32_t *dist, 
+                         uint32_t max_length, size_t *index) {
+  uint32_t max_index = *index;
+  uint32_t cur_length = max_length;
+  size_t prev_index = *index;
+  size_t cur_index = prev_index;
+  
+  while (dist[cur_index] != 1) {
+    if (dist[cur_index] != cur_length - 1 &&
+        seq[prev_index] >= seq[cur_index]) {
+      --cur_index;
+    }
+    else {
+      prev_index = cur_index;
+      --cur_length;
+    }
+  }
+
+  *index = cur_index;
+  // find the first possible element in an non-decreasing sequence
+  for (size_t i = cur_index - 1; i < cur_index; --i) {
+    if (dist[i] == 1 && seq[i] < seq[*index] && 
+        seq[i] <= seq[max_index]) {
+      *index = i;
+    }
+  }
 }
