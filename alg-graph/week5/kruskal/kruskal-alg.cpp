c99c713556004d7bfdb83e9e567ef662f7c0d4a1
Vladimir Petrigo <taenaru@gmail.com>
2016-09-01 01:44:40 +0300

    dummy implementation

--- alg-graph/week5/kruskal/kruskal-alg.cpp
+++ alg-graph/week5/kruskal/kruskal-alg.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <cassert>
 #include <cmath>
 #include <tuple>
+#include <algorithm>
+#include <iterator>
 #include "disjoint_set.hpp"
 
 using TPoint = std::pair<int, int>;
@@ -12,15 +15,19 @@ using TEdge = std::tuple<TVertice, TVertice, double>;
 std::vector<TPoint> read_coords(unsigned int n);
 std::vector<TEdge> create_edge_list(const std::vector<TPoint>& coords);
 double calculate_length(const TPoint& p1, const TPoint& p2);
+double make_clusters(const unsigned n, const unsigned k, const std::vector<TEdge>& edges_list);
 
 int main() {
-  unsigned n;
+  unsigned n, k;
 
   std::cin >> n;
 
   auto coords = read_coords(n);
-
-  create_edge_list(coords);
+  auto edges_list = create_edge_list(coords);
+  
+  std::cin >> k;
+  std::cout << std::fixed << std::setprecision(9) << 
+    make_clusters(n, k, edges_list) << std::endl;
 
   return 0;
 }
@@ -61,4 +68,44 @@ double calculate_length(const TPoint& p1, const TPoint& p2) {
   auto diff2 = p1.second - p2.second;
 
   return std::sqrt(diff1 * diff1 + diff2 * diff2);
+}
+
+double make_clusters(const unsigned n, const unsigned k, const std::vector<TEdge>& edges_list) {
+  DisjointSet clusters{n};
+  std::vector<TEdge> sorted_edges(edges_list);
+  std::size_t clusters_count = 0;
+  constexpr auto UNKNOWN_CLUSTER = 256U;
+  TVertice prev_cluster = UNKNOWN_CLUSTER;
+  bool loop_found = false;
+  auto it = std::begin(sorted_edges);
+  
+  std::sort(std::begin(sorted_edges), std::end(sorted_edges), 
+            [](const TEdge& a, const TEdge&b) { return std::get<2>(a) < std::get<2>(b); });
+
+  std::for_each(std::begin(sorted_edges), std::end(sorted_edges), [](const TEdge& e) {
+    std::cout << "U: " << std::get<0>(e) << " V: " << std::get<1>(e) <<
+      " W: " << std::get<2>(e) << std::endl; 
+  });
+
+  while (clusters_count <= k && it != std::end(edges_list)) {
+    auto u = std::get<0>(*it);
+    auto v = std::get<1>(*it);
+    auto cluster1 = clusters.Find(u);
+    auto cluster2 = clusters.Find(v);
+
+    std::cout << "prev cluster: " << prev_cluster << std::endl;
+    if (prev_cluster == UNKNOWN_CLUSTER || cluster1 != cluster2) {
+      if (cluster1 != prev_cluster && cluster2 != prev_cluster) {
+        ++clusters_count;
+      }
+      clusters.Unite(u, v);
+      prev_cluster = clusters.Find(u);
+    }
+    std::cout << "clusters: " << clusters_count << std::endl;
+    std::cout << "prev cluster: " << prev_cluster << std::endl;
+
+    ++it;
+  }
+  
+  return std::get<2>(*it);
 }
