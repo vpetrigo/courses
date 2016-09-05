cf99e0d28f2e25f982b23f38bc9dc70a5b2e9c0b
Vladimir Petrigo <taenaru@gmail.com>
2016-09-05 18:47:30 +0300

    start refactoring

--- alg-graph/week2/scc/scc.cpp
+++ alg-graph/week2/scc/scc.cpp
@@ -3,20 +3,26 @@
 #include <stack>
 #include <utility>
 #include <functional>
+#include <algorithm>
 
-using GraphRepr = std::vector<std::vector<int>>;
-using ProcessStack = std::stack<int>;
+using VerticeType_t = unsigned;
+using GraphRepr = std::vector<std::vector<VerticeType_t>>;
+using NeighbourIt_t = GraphRepr::value_type::const_iterator;
+using ProcessStack = std::stack<std::pair<VerticeType_t, NeighbourIt_t>>;
+// pair of pre- and post-times
 using VerticeTimeInfo = std::pair<unsigned, unsigned>;
 
-void fill_graph(int edges, GraphRepr& graph); 
+enum class VerticeStatus { UNVISITED, PROCESSING, VISITED };
+
+void fill_graph(std::size_t edges, GraphRepr& graph);
 GraphRepr make_reverse_graph(const GraphRepr& graph);
 std::vector<VerticeTimeInfo> dfs(const GraphRepr& graph);
-void explore(const GraphRepr& graph, int vertice, std::vector<bool>& visited,
+void explore(const GraphRepr& graph, int vertice, std::vector<VerticeStatus>& visited,
              std::vector<VerticeTimeInfo> *time_info = nullptr, int *time = nullptr);
 int count_conn_components(const GraphRepr& graph, const std::vector<VerticeTimeInfo>& time_inf);
 
 int main() {
-    int n, m;
+    std::size_t n, m;
     std::cin >> n >> m;
     GraphRepr graph(n);
 
@@ -24,13 +30,14 @@ int main() {
     auto graph_r = make_reverse_graph(graph);
     auto time_info = dfs(graph_r);
     std::cout << count_conn_components(graph, time_info) << std::endl;
-    
+
     return 0;
 }
 
-void fill_graph(int edges, GraphRepr& graph) {
+void fill_graph(std::size_t edges, GraphRepr& graph) {
     for (std::size_t i = 0; i < edges; ++i) {
-        int u, v;
+        VerticeType_t u, v;
+
         std::cin >> u >> v;
         graph[u - 1].emplace_back(v - 1);
     }
@@ -39,7 +46,7 @@ void fill_graph(int edges, GraphRepr& graph) {
 GraphRepr make_reverse_graph(const GraphRepr& graph) {
     auto graph_len = graph.size();
     GraphRepr graph_reverse(graph_len);
-    
+
     for (std::size_t i = 0; i < graph_len; ++i) {
        for (const auto& vertice : graph[i]) {
            graph_reverse[vertice].emplace_back(i);
@@ -53,10 +60,10 @@ std::vector<VerticeTimeInfo> dfs(const GraphRepr& graph) {
     int time = 1;
     auto graph_len = graph.size();
     std::vector<VerticeTimeInfo> time_inf(graph_len, std::make_pair(0, 0));
-    std::vector<bool> visited(graph_len, false);
+    std::vector<VerticeStatus> visited(graph_len, VerticeStatus::UNVISITED);
 
     for (std::size_t i = 0; i < graph_len; ++i) {
-        if (!visited[i]) {
+        if (visited[i] == VerticeStatus::UNVISITED) {
             explore(graph, i, visited, &time_inf, &time);
         }
     }
@@ -64,42 +71,42 @@ std::vector<VerticeTimeInfo> dfs(const GraphRepr& graph) {
     return time_inf;
 }
 
-void explore(const GraphRepr& graph, int vertice, std::vector<bool>& visited,
+void explore(const GraphRepr& graph, int vertice, std::vector<VerticeStatus>& visited,
              std::vector<VerticeTimeInfo> *time_info, int *time) {
     ProcessStack proc_stack;
 
-    proc_stack.push(vertice);
+    proc_stack.push(std::make_pair(vertice, graph[vertice].cbegin()));
 
     while (!proc_stack.empty()) {
         auto proc_vertice = proc_stack.top();
 
-        if (!visited[proc_vertice]) {
-            std::cout << "PV: " << proc_vertice + 1;
+        if (visited[proc_vertice.first]) {
+            std::cout << "PV: " << proc_vertice.first + 1;
             if (time) {
                 std::cout << " pre time: " << *time;
             }
             std::cout << std::endl;
-            visited[proc_vertice] = true;
+            visited[proc_vertice.first] = true;
             // fill previsited time
             if (time_info && time) {
-                (*time_info)[proc_vertice].first = (*time)++;
+                (*time_info)[proc_vertice.first].first = (*time)++;
             }
 
-            for (const auto& neighbour : graph[proc_vertice]) {
+            for (const auto& neighbour : graph[proc_vertice.first]) {
                 if (!visited[neighbour]) {
-                    proc_stack.push(neighbour);
+                    proc_stack.push(std::make_pair(neighbour, graph[neighbour].cbegin()));
                 }
             }
         }
         else {
-            std::cout << "PV: " << proc_vertice + 1;
+            std::cout << "PV: " << proc_vertice.first + 1;
             if (time) {
                 std::cout << " post time: " << *time ;
             }
             std::cout << std::endl;
             // fill postvisited time
             if (time_info && time) {
-                (*time_info)[proc_vertice].second = (*time)++;
+                (*time_info)[proc_vertice.first].second = (*time)++;
             }
             proc_stack.pop();
         }
@@ -109,7 +116,7 @@ void explore(const GraphRepr& graph, int vertice, std::vector<bool>& visited,
 int count_conn_components(const GraphRepr& graph, const std::vector<VerticeTimeInfo>& time_info) {
     auto graph_len = graph.size();
     std::vector<std::pair<int, int>> vertice_post(graph_len);
-    std::vector<bool> visited(graph_len, false);
+    std::vector<VerticeStatus> visited(graph_len, VerticeStatus::UNVISITED);
     unsigned conn_comp_counter = 0;
 
     for (std::size_t i = 0; i < graph_len; ++i) {
@@ -118,12 +125,11 @@ int count_conn_components(const GraphRepr& graph, const std::vector<VerticeTimeI
 
     std::sort(vertice_post.begin(), vertice_post.end(), std::greater<std::pair<int, int>> ());
     for (std::size_t v = 0; v < graph_len; ++v) {
-        if (!visited[vertice_post[v].second]) {
+        if (visited[vertice_post[v].second] == VerticeStatus::UNVISITED) {
             ++conn_comp_counter;
-            explore(graph, vertice_post[v].second, visited);  
+            explore(graph, vertice_post[v].second, visited);
         }
     }
-    
+
     return conn_comp_counter;
 }
-
