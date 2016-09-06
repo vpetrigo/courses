a442a60bd0c6e410ef68f96762c6221cf20c1628
Vladimir Petrigo <taenaru@gmail.com>
2016-09-06 13:24:56 +0300

    solution for the digits sum problem

---
+++ programming/adaptive-python/group_2/digits_sum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+def get_digit(num):
+    digit_num = 0
+
+    while num > 0:
+        digit_num = num % 10
+        yield digit_num
+        num //= 10
+
+    return digit_num
+
+
+def main():
+    n = int(input())
+    print(sum(get_digit(n)))
+
+if __name__ == "__main__":
+    main()
