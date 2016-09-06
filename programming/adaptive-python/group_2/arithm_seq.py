2fdc95be0a91787bd31a04b3138c109ab95682d5
Vladimir Petrigo <taenaru@gmail.com>
2016-09-06 16:05:33 +0300

    Solution for the Arithmetic sequence problem

---
+++ programming/adaptive-python/group_2/arithm_seq.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+import sys
+
+
+def is_arithm_seq(seq):
+    if len(seq) >= 2:
+        d = seq[1] - seq[0]
+        a_1 = seq[0]
+
+        for i in range(1, len(seq)):
+            if seq[i] != a_1 + i * d:
+                return False
+        else:
+            return True
+
+    return True
+        
+
+def main():
+    sequence = list(map(int, sys.stdin.readline().split()))
+    sequence = sorted(sequence)
+
+    if is_arithm_seq(sequence):
+        print("Yes")
+    else:
+        print("No")
+    
+
+if __name__ == "__main__":
+    main()
