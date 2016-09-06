83d5f4589e0d424de8eb76346cb95017414347d6
Vladimir Petrigo <taenaru@gmail.com>
2016-09-06 18:06:24 +0300

    add some more solutions for the simple problems

---
+++ programming/adaptive-python/group_2/clock.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+
+def main():
+    seconds = int(input())
+    clock_s = seconds % 60
+    clock_m = seconds // 60 % 60
+    clock_h = seconds // 60 // 60 % 24
+
+    print("{}:{:02}:{:02}".format(clock_h, clock_m, clock_s))
+
+if __name__ == "__main__":
+    main()
---
+++ programming/adaptive-python/group_2/squirrel_nut_eq.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+def main():
+    squrrels = int(input())
+    nuts = int(input())
+
+    print(nuts % squrrels)
+
+if __name__ == "__main__":
+    main()
