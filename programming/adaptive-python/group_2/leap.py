dcb8a67e86091823432c7b8b3d3129fc0e33cc9b
Vladimir Petrigo <taenaru@gmail.com>
2016-09-06 16:18:48 +0300

    Solution for the Leap year problem

---
+++ programming/adaptive-python/group_2/leap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+def is_leap(year):
+    if (year % 400 == 0 or
+            (year % 4 == 0 and not year % 100 == 0)):
+        return True
+    return False
+
+
+def main():
+    year = int(input())
+
+    if not is_leap(year):
+        print("Regular")
+    else:
+        print("Leap")
+
+if __name__ == "__main__":
+    main()
