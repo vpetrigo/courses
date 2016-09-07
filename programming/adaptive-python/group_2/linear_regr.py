2e0f98328a3593ba7101ea614c44f78207650fbb
Vladimir Petrigo <taenaru@gmail.com>
2016-09-08 02:04:29 +0300

    Solution for the Linear Regression problem

--- programming/adaptive-python/group_2/linear_regr.py
+++ programming/adaptive-python/group_2/linear_regr.py
@@ -12,10 +12,11 @@ def main():
     data = np.loadtxt(file, delimiter=",", skiprows=1)
     y, *factors = np.hsplit(data, len(data[0]))
     y_1 = np.hstack(y)
-    A = np.concatenate(tuple(np.hstack(fact) for fact in factors))
-    print(A)
+    A = np.stack((np.hstack(fact) for fact in factors))
     A = np.concatenate((A, [np.ones_like(y_1)]))
-    print(" ".join(map(str, np.linalg.lstsq(A.T, y_1)[0])))
+    b = np.linalg.lstsq(A.T, y_1)[0]
+    b = np.concatenate(([b[-1]], b[:-1]))
+    print(" ".join(map(str, b)))
 
 
 if __name__ == "__main__":
