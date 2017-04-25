89e4c8a93030642517b8cb13c15ef87a38d335fa
Vladimir Petrigo <taenaru@gmail.com>
2017-04-25 23:42:02 +0300

    Add method for accessing back element of a Queue

--- alg-datastruct/week1/network_queue.h
+++ alg-datastruct/week1/network_queue.h
@@ -3,62 +3,66 @@
 
 #include <memory>
 
-template<typename T>
+template <typename T>
 class NetworkQueue {
  public:
-  explicit NetworkQueue(const std::size_t queue_size) : queue_{std::make_unique<T[]>(queue_size)},
-                                                        queue_indexes_{0, 0},
-                                                        queue_size_{0},
-                                                        queue_capacity_{queue_size} {}
+  explicit NetworkQueue(const std::size_t queue_size)
+      : queue_{std::make_unique<T[]>(queue_size)},
+        queue_indexes_{0, 0},
+        queue_size_{0},
+        queue_capacity_{queue_size}
+  {
+  }
 
   ~NetworkQueue() = default;
 
-  NetworkQueue(const NetworkQueue &nq) = delete;
-  NetworkQueue& operator=(const NetworkQueue &nq) = delete;
+  NetworkQueue(const NetworkQueue& nq) = delete;
+  NetworkQueue& operator=(const NetworkQueue& nq) = delete;
 
-  NetworkQueue(NetworkQueue &&nq) noexcept = default;
-  NetworkQueue& operator=(NetworkQueue &&nq) noexcept = default;
+  NetworkQueue(NetworkQueue&& nq) noexcept = default;
+  NetworkQueue& operator=(NetworkQueue&& nq) noexcept = default;
 
-  bool Empty() const {
-    return (queue_size_ == 0);
-  }
-
-  bool Full() const {
-    return (queue_size_ == queue_capacity_);
-  }
-
-  std::size_t Size() const {
-    return queue_size_;
-  }
-
-  void Push(const T &el) {
+  bool Empty() const { return (queue_size_ == 0); }
+  bool Full() const { return (queue_size_ == queue_capacity_); }
+  std::size_t Size() const { return queue_size_; }
+  void Push(const T& el)
+  {
     queue_[queue_indexes_.second] = el;
-    queue_indexes_.second = (queue_indexes_.second + 1)%queue_capacity_;
+    queue_indexes_.second = (queue_indexes_.second + 1) % queue_capacity_;
     ++queue_size_;
   }
 
-  void Push(T&& el) {
+  void Push(T&& el)
+  {
     queue_[queue_indexes_.second] = std::move(el);
-    queue_indexes_.second = (queue_indexes_.second + 1)%queue_capacity_;
+    queue_indexes_.second = (queue_indexes_.second + 1) % queue_capacity_;
     ++queue_size_;
   }
 
-  const T& Top() const {
-    return queue_[queue_indexes_.first];
+  const T& Top() const { return queue_[queue_indexes_.first]; }
+  T& Top() { return queue_[queue_indexes_.first]; };
+  const T& Back() const
+  {
+    return queue_[(queue_indexes_.second == 0) ? queue_capacity_ - 1
+                                               : queue_indexes_.second - 1];
   }
 
-  T& Top() {
-    return queue_[queue_indexes_.first];
-  };
+  T& Back()
+  {
+    return queue_[(queue_indexes_.second == 0) ? queue_capacity_ - 1
+                                               : queue_indexes_.second - 1];
+  }
 
-  T Pop() {
+  T Pop()
+  {
     T ret{queue_[queue_indexes_.first]};
 
-    queue_indexes_.first = (queue_indexes_.first + 1)%queue_capacity_;
+    queue_indexes_.first = (queue_indexes_.first + 1) % queue_capacity_;
     --queue_size_;
 
     return ret;
   }
+
  private:
   std::unique_ptr<T[]> queue_;
   std::pair<std::size_t, std::size_t> queue_indexes_;
@@ -66,4 +70,4 @@ class NetworkQueue {
   std::size_t queue_capacity_;
 };
 
-#endif // _NETWORK_QUEUE_H_
+#endif  // _NETWORK_QUEUE_H_
