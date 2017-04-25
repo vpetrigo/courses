ac1772966db9c24069e5efdb7f2c4be5169c5d7b
Vladimir Petrigo <taenaru@gmail.com>
2017-04-25 23:42:25 +0300

    Add solution for the Network Processor problem

--- alg-datastruct/week1/network_queue.cpp
+++ alg-datastruct/week1/network_queue.cpp
@@ -3,56 +3,36 @@
 #include <iostream>
 
 using NetworkPacket = std::pair<std::size_t, std::size_t>;
-using NumNetworkQueue = NetworkQueue<NetworkPacket>;
+using NumNetworkQueue = NetworkQueue<std::size_t>;
 
-static void handle_processed_packets(const NetworkPacket& packet, std::size_t& cur_time, NumNetworkQueue& queue) {
-  auto& top_packet = queue.Top();
-
-  while (!queue.Empty() && packet.first >= (top_packet.first + top_packet.second)) {
-    cur_time += top_packet.first + top_packet.second;
-    queue.Pop();
-    std::cout << cur_time << std::endl;
-    top_packet = queue.Top();
-  }
-}
-
-int main() {
+int main()
+{
   std::size_t queue_size, packet_number;
+  constexpr int QUEUE_IS_FULL = -1;
 
   std::cin >> queue_size >> packet_number;
 
   NumNetworkQueue n_queue{queue_size};
-  std::size_t cur_time = 0;
   NetworkPacket packet{0, 0};
 
   for (std::size_t i = 0; i < packet_number; ++i) {
     std::cin >> packet.first >> packet.second;
 
+    while (!n_queue.Empty() && packet.first >= n_queue.Top()) {
+      // process packets that can be processed before a new packet
+      n_queue.Pop();
+    }
+
     if (!n_queue.Full()) {
-      n_queue.Push(packet);
+      std::size_t process_start = (n_queue.Empty()) ? packet.first : n_queue.Back();
+      std::size_t process_end = process_start + packet.second;
+
+      n_queue.Push(process_end);
+      std::cout << process_start << std::endl;
     }
     else {
-      /*auto& top_packet = n_queue.Top();
-
-      while (!n_queue.Empty() && packet.first >= (top_packet.first + top_packet.second)) {
-        cur_time += top_packet.first + top_packet.second;
-        n_queue.Pop();
-        std::cout << cur_time << std::endl;
-        top_packet = n_queue.Top();
-      }*/
-      handle_processed_packets(packet, cur_time, n_queue);
-
-      if (n_queue.Full()) {
-        std::cout << -1 << std::endl;
-      }
-      else {
-        n_queue.Push(packet);
-      }
+      std::cout << QUEUE_IS_FULL << std::endl;
     }
-  }
-
-  while (!n_queue.Empty()) {
-
   }
 
   return 0;
