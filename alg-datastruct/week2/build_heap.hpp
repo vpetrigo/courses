c4d2cf2201049268cff501b49f7d90467df74e93
Vladimir Petrigo <taenaru@gmail.com>
2017-04-28 09:04:36 +0300

    Clean up code format

--- alg-datastruct/week2/build_heap.hpp
+++ alg-datastruct/week2/build_heap.hpp
@@ -15,11 +15,13 @@ void SiftDown(It first, It last, const std::size_t length, std::size_t index)
     left_child = 2 * index + 1;
     right_child = 2 * index + 2;
 
-    if (left_child < length && *(first + left_child) < *(first + smallest_elem)) {
+    if (left_child < length &&
+        *(first + left_child) < *(first + smallest_elem)) {
       smallest_elem = left_child;
     }
 
-    if (right_child < length && *(first + right_child) < *(first + smallest_elem)) {
+    if (right_child < length &&
+        *(first + right_child) < *(first + smallest_elem)) {
       smallest_elem = right_child;
     }
 
