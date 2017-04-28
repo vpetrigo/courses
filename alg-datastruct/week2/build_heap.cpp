b69293bb035ebb831162112fbcd608713fdbbaff
Vladimir Petrigo <taenaru@gmail.com>
2017-04-28 23:48:07 +0300

    Add solution for the Heap Builder problem

--- alg-datastruct/week2/build_heap.cpp
+++ alg-datastruct/week2/build_heap.cpp
@@ -1,9 +1,30 @@
+/*
+ * Проблема: Построение кучи
+ * Переставить элементы заданного массива чисел так, чтобы
+ * он удовлетворял свойству мин-кучи.
+ * Вход: массив чисел A[0 ... n - 1]
+ * Выход: переставить элементы массива так, чтобы выполнялись
+ * неравенства A[i] <= A[2i + 1] и A[i] <= A[2i + 2] для всех i
+ *
+ * Пример:
+ *  Вход:
+ *  5
+ *  5 4 3 2 1
+ *  Выход:
+ *  3
+ *  1 4
+ *  0 1
+ *  1 3
+ *
+ */
 #include "build_heap.hpp"
 
+#include <iostream>
 #include <vector>
 
 int main() {
   std::size_t input_size;
+  HeapBuilder<std::size_t> hp;
 
   std::cin >> input_size;
 
@@ -13,7 +34,13 @@ int main() {
     std::cin >> elem;
   }
 
-  BuildHeap(numbers.begin(), numbers.end());
+  hp.BuildHeap(numbers.begin(), numbers.end());
+  const auto& rearrange = hp.Rearrange();
+  std::cout << rearrange.size() << std::endl;
+
+  for (const auto& elem : rearrange) {
+    std::cout << elem.first << ' ' << elem.second << std::endl;
+  }
 
   return 0;
 }
--- alg-datastruct/week2/build_heap.hpp
+++ alg-datastruct/week2/build_heap.hpp
@@ -2,8 +2,8 @@
 #define _BUILD_HEAP_HPP_
 
 #include <iterator>
-#include <vector>
 #include <utility>
+#include <vector>
 
 template <typename It, typename Cmp>
 void SiftDown(It first, It last, const std::size_t length, const Cmp& compare,
@@ -36,12 +36,14 @@ void SiftDown(It first, It last, const std::size_t length, const Cmp& compare,
   }
 }
 
-template <typename It, typename Cmp = std::less<typename std::iterator_traits<It>::value_type>>
+template <typename It, typename Cmp = std::less<
+                           typename std::iterator_traits<It>::value_type>>
 void BuildHeap(It first, It last, const Cmp& compare = Cmp{})
 {
   std::size_t length = static_cast<std::size_t>(std::distance(first, last));
 
-  for (std::size_t i = static_cast<std::size_t>(length / 2); i <= length / 2; --i) {
+  for (std::size_t i = static_cast<std::size_t>(length / 2); i <= length / 2;
+       --i) {
     SiftDown(first, last, length, compare, i);
   }
 }
@@ -49,7 +51,9 @@ void BuildHeap(It first, It last, const Cmp& compare = Cmp{})
 template <typename T, typename Compare = std::less<T>>
 class HeapBuilder {
  public:
-  explicit HeapBuilder(const Compare& compare = Compare{}) : compare_{compare} {}
+  explicit HeapBuilder(const Compare& compare = Compare{}) : compare_{compare}
+  {
+  }
 
   virtual ~HeapBuilder() = default;
 
@@ -60,7 +64,8 @@ class HeapBuilder {
   HeapBuilder& operator=(HeapBuilder&& hb) noexcept = default;
 
   template <typename It>
-  void BuildHeap(It first, It last) {
+  void BuildHeap(It first, It last)
+  {
     std::size_t length = static_cast<std::size_t>(std::distance(first, last));
     rearrange_.clear();
 
@@ -69,12 +74,15 @@ class HeapBuilder {
     }
   }
 
-  const std::vector<std::pair<std::size_t, std::size_t>>& Rearrange() const {
+  const std::vector<std::pair<std::size_t, std::size_t>>& Rearrange() const
+  {
     return rearrange_;
   };
+
  protected:
   template <typename It>
-  void SiftDown(It first, It last, const std::size_t length, std::size_t index) {
+  void SiftDown(It first, It last, const std::size_t length, std::size_t index)
+  {
     auto smallest_index = index;
     bool finish = false;
 
@@ -82,11 +90,13 @@ class HeapBuilder {
       auto left_child = 2 * index + 1;
       auto right_child = 2 * index + 2;
 
-      if (left_child < length && compare_(*(first + left_child), *(first + smallest_index))) {
+      if (left_child < length &&
+          compare_(*(first + left_child), *(first + smallest_index))) {
         smallest_index = left_child;
       }
 
-      if (right_child < length && compare_(*(first + right_child), *(first + smallest_index))) {
+      if (right_child < length &&
+          compare_(*(first + right_child), *(first + smallest_index))) {
         smallest_index = right_child;
       }
 
@@ -100,6 +110,7 @@ class HeapBuilder {
       }
     }
   }
+
  private:
   Compare compare_;
   std::vector<std::pair<std::size_t, std::size_t>> rearrange_;
