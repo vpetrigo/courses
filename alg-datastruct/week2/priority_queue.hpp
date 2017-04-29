8df513cd6f8dafb1927386cee82946d05dcfe09b
Vladimir Petrigo <taenaru@gmail.com>
2017-04-29 17:38:00 +0300

    Add Priority Queue interface
    Add tests for small array and big random array

---
+++ alg-datastruct/week2/priority_queue.hpp
@@ -0,0 +1,85 @@
+#ifndef _PRIORITY_QUEUE_HPP_
+#define _PRIORITY_QUEUE_HPP_
+
+#include <functional>
+#include <iostream>
+#include <vector>
+
+template <typename T, typename Compare = std::less<T>>
+class PriorityQueue {
+ public:
+  explicit PriorityQueue(const Compare& compare = Compare{})
+      : compare_{compare}, heap_{}
+  {
+  }
+
+  void AddElement(T&& element)
+  {
+    heap_.emplace_back(std::move(element));
+    SiftUp(heap_.size() - 1);
+  }
+
+  void AddElement(const T& element)
+  {
+    heap_.push_back(element);
+    SiftUp(heap_.size() - 1);
+  }
+
+  T ExtractTop()
+  {
+    T top{heap_.front()};
+
+    std::swap(heap_.front(), heap_.back());
+    heap_.pop_back();
+    SiftDown(0);
+
+    return top;
+  }
+
+ protected:
+  std::size_t Parent(std::size_t index) { return (index - 1) / 2; }
+  std::size_t LeftChild(std::size_t index) { return 2 * index + 1; }
+  std::size_t RightChild(std::size_t index) { return 2 * index + 2; }
+  void SiftUp(std::size_t index)
+  {
+    while (index > 0 && compare_(heap_[Parent(index)], heap_[index])) {
+      std::swap(heap_[Parent(index)], heap_[index]);
+      index = Parent(index);
+    }
+  }
+
+  void SiftDown(std::size_t index)
+  {
+    bool finish = false;
+    std::size_t smallest_index = index;
+
+    while (!finish) {
+      auto l_child = LeftChild(index);
+      auto r_child = RightChild(index);
+
+      if (l_child < heap_.size() &&
+          compare_(heap_[smallest_index], heap_[l_child])) {
+        smallest_index = l_child;
+      }
+
+      if (r_child < heap_.size() &&
+          compare_(heap_[smallest_index], heap_[r_child])) {
+        smallest_index = r_child;
+      }
+
+      if (smallest_index != index) {
+        std::swap(heap_[smallest_index], heap_[index]);
+        index = smallest_index;
+      }
+      else {
+        finish = true;
+      }
+    }
+  }
+
+ private:
+  Compare compare_;
+  std::vector<T> heap_;
+};
+
+#endif  // _PRIORITY_QUEUE_HPP_
---
+++ alg-datastruct/week2/priority_queue_test.cpp
@@ -0,0 +1,40 @@
+#include "priority_queue.hpp"
+
+#include <algorithm>
+#include <random>
+#include <gtest/gtest.h>
+
+TEST(Priority_Queue, small_heap_sort) {
+  constexpr std::size_t SIZE = 3;
+  PriorityQueue<std::size_t, std::greater<std::size_t>> queue;
+  std::array<std::size_t, SIZE> arr;
+
+  for (std::size_t i = SIZE - 1; i <= SIZE; --i) {
+    queue.AddElement(i);
+  }
+
+  for (std::size_t i = 0; i < SIZE; ++i) {
+    arr[i] = queue.ExtractTop();
+  }
+
+  ASSERT_TRUE(std::is_sorted(arr.cbegin(), arr.cend()));
+}
+
+TEST(Priority_Queue, big_random_array_sort) {
+  constexpr std::size_t SIZE = 1024;
+  std::array<std::size_t, SIZE> arr;
+  PriorityQueue<std::size_t, std::greater<std::size_t>> queue;
+  std::random_device rd{};
+  std::mt19937 gen(rd());
+  std::binomial_distribution<std::size_t> dist{4096, 0.4};
+
+  for (std::size_t i = 0; i < SIZE; ++i) {
+    queue.AddElement(dist(gen));
+  }
+
+  for (auto& elem : arr) {
+    elem = queue.ExtractTop();
+  }
+
+  ASSERT_TRUE(std::is_sorted(arr.cbegin(), arr.cend()));
+}
