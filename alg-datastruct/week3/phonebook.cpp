bb44f9907fee54705a87b6355b26a3b60344036b
Vladimir Petrigo <taenaru@gmail.com>
2017-05-13 12:48:34 +0300

    Add solution for the Phonebook problem

--- alg-datastruct/week3/phonebook.cpp
+++ alg-datastruct/week3/phonebook.cpp
@@ -1,5 +1,44 @@
-//
-// Created by vladimir.petrigo on 11.05.2017.
-//
-
 #include "phonebook.hpp"
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::size_t n_queries;
+  std::string command;
+  NumericHashTable ht{1024};
+  std::pair<std::size_t, std::string> query_storage;
+
+  std::cin >> n_queries;
+
+  while (n_queries-- > 0) {
+    std::cin >> command;
+
+    if (command.compare("add") == 0) {
+      std::cin >> query_storage.first >> query_storage.second;
+      ht.Add(std::make_pair(query_storage.first, query_storage.second));
+    }
+    else if (command.compare("find") == 0) {
+      std::cin >> query_storage.first;
+
+      auto elem_it = ht.Find(query_storage.first);
+
+      if (elem_it != ht.End()) {
+        std::cout << elem_it->second;
+      }
+      else {
+        std::cout << "not found";
+      }
+
+      std::cout << std::endl;
+    }
+    else {
+      // delete request
+      std::cin >> query_storage.first;
+      ht.Delete(query_storage.first);
+    }
+  }
+
+  return 0;
+}
