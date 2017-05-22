783236b2996b6019392bf3f4b1d2540e23f193e0
Vladimir Petrigo <taenaru@gmail.com>
2017-05-22 21:12:34 +0300

    Update std_sort.cpp

--- alg-sort/radix/std_sort.cpp
+++ alg-sort/radix/std_sort.cpp
@@ -7,7 +7,8 @@ int main() {
     int n, base;
 
     std::cin >> n >> base;
-    std::istream_iterator<unsigned int> it(std::cin);
+    
+    std::istream_iterator<unsigned int> it{std::cin};
     std::istream_iterator<unsigned int> sent;
     std::vector<unsigned int> inp(it, sent);
 
