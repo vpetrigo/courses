773cbe67f3b956d3c496a291cac32d52284b9293
Vladimir Petrigo <taenaru@gmail.com>
2017-06-26 19:04:19 +0300

    Add solution for the phone number problem

---
+++ programming/adaptive-python/group_2_2/phone_num.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+import re
+
+
+def main():
+    pnum = re.fullmatch("^19\d{9}$", input())
+    print("Yes" if pnum else "No")  
+
+
+if __name__ == "__main__":
+    main()
