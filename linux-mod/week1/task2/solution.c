ba04246195adefb14ae1b7bbe2175b64adf64f04
Vladimir Petrigo <taenaru@gmail.com>
2017-07-01 18:37:27 +0300

    week1/task2: template for task2

---
+++ linux-mod/week1/task2/solution.c
@@ -0,0 +1,24 @@
+#include "checker.h"
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+
+MODULE_AUTHOR("Vladimir Petrigo");
+MODULE_LICENSE("Dual BSD/GPL");
+
+int __init solution_init(void)
+{
+	pr_info("solution: [kernel_mooc] module initialization\n");
+	call_me("Hello from my module!");
+
+	return 0;
+}
+
+void __exit solution_exit(void)
+{
+	pr_info("solution: [kernel_mooc] module exit\n");
+}
+
+module_init(solution_init);
+module_exit(solution_exit);
