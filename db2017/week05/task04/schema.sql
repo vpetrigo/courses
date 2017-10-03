7ffe448e47e049583152978aa94bc769a30217ca
Vladimir Petrigo <taenaru@gmail.com>
2017-10-03 22:11:59 +0300

    db2017: Add schema and container runner script for week05 task04

---
+++ db2017/week05/task04/schema.sql
@@ -0,0 +1,70 @@
+-- database sample for task 5.4
+
+DROP TABLE IF EXISTS StockQuotes;
+
+CREATE TABLE StockQuotes(
+  company TEXT,
+  week INT,
+  share_price INT
+);
+
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  1,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  2,  15);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  3,  20);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  4,  25);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  5,  30);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  6,  35);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  7,  40);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  8,  45);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple',  9,  50);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 10,  60);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 11,  70);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 12,  80);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 13,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 14,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 15, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 16, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 17, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 18, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 19, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Apple', 20,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  1,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  2,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  3,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  4,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  5,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  6,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  7,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  8,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  9,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  10,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  11,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  12,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  13,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  14,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  15,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  16,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  17,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  18,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  19,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Oracle',  20,  10);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  1, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  2,  95);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  3,  95);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  4,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  5,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  6,  85);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  7,  80);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  8,  100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  9,  120);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  10, 150);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  11, 100);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  12,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  13,  90);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  14,  85);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  15,  80);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  16,  75);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  17,  70);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  18,  70);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  19,  65);
+INSERT INTO StockQuotes (company, week, share_price) VALUES ('Microsoft',  20,  60);
