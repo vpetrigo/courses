b6aa04af2191d1875af3eea690188275683e6684
Vladimir Petrigo <taenaru@gmail.com>
2017-10-22 18:02:21 +0300

    java: Add solution for week03 task03 + tests

---
+++ java/week03/task03/TestWithBadMock.java
@@ -0,0 +1,28 @@
+import robot.RobotConnectionManagerBadMock;
+import robot.RobotConnectionException;
+import robot.RobotConnectionManager;
+import robot.RobotConnection;
+
+public class TestWithBadMock {
+    public static void main(String[] args) {
+        moveRobot(new RobotConnectionManagerBadMock(), 1, 2);
+    }
+
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        final int RETRY_NUM = 3;
+        boolean connectionEstablished = false;
+        RobotConnection rc = robotConnectionManager.getConnection();
+
+        for (int i = 0; i < RETRY_NUM && !connectionEstablished; ++i) {
+            try (rc) {
+                rc.moveRobotTo(toX, toY);
+                connectionEstablished = true;
+            }
+            catch (RobotConnectionException re) {
+                if (i == RETRY_NUM - 1) {
+                    throw re;
+                }
+            }
+        }
+    }
+}
