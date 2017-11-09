9686c0abce115113e7c00309d8c556b124c81bff
Vladimir Petrigo <taenaru@gmail.com>
2017-11-09 08:14:32 +0300

    java: Add Spy class implementation

---
+++ java/week04/task02/Spy.java
@@ -0,0 +1,31 @@
+import java.util.logging.Logger;
+
+public class Spy implements MailService {
+    private Logger logger;
+    public static final String AUSTIN_POWERS = "Austin Powers";
+
+    public Spy(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Override
+    Sendable processSendable(Sendable mail) {
+        if (mail instanceof MailMessage) {
+            MailMessage test = (MailMessage) mail;
+
+            if (test.getTo().equals(AUSTIN_POWERS)) {
+                String warn = String.format("Detected target mail correspondence: " + 
+                                            "from %s to %s \"%s\"", test.getFrom(),
+                                            test.getTo(), test.getMessage());
+                logger.warning(warn);
+            }
+            else {
+                String info = String.format("Usual correspondence: from %s to %s", 
+                                            test.getFrom(), test.getTo());
+                logger.info(info);
+            }
+        }
+
+        return mail;
+    }
+}
