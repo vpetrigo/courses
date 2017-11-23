27edfd154706a3b851110817ba5f3496a16898f4
Vladimir Petrigo <taenaru@gmail.com>
2017-11-23 21:39:44 +0300

    db2017: Add docker starter for week09 task02

---
+++ db2017/week09/task02/task-prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONTAINER_NAME=postgres-task
+
+docker run --rm --name ${CONTAINER_NAME} -d -p 5432:5432 -v $(pwd):/workspace postgres:9.6
+sleep 10
+docker exec ${CONTAINER_NAME} psql -U postgres -f /workspace/schema.sql
