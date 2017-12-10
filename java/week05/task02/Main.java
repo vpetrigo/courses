37e737c8f7a6c6adba90ff7f6d66f898deb734c7
Vladimir Petrigo <taenaru@gmail.com>
2017-12-10 07:15:34 +0300

    java: Modify solution for week05 task02 to use buffers

--- java/week05/task02/Main.java
+++ java/week05/task02/Main.java
@@ -1,18 +1,47 @@
 import java.io.IOException;
 
 public class Main {
-    public static final byte CAR_RET  = 13;
-    public static final byte NEW_LINE = 10;
+    public static final byte CR  = '\r';
+    public static final byte LF = '\n';
+    private static boolean last_cr = false;
     
     public static void main(String[] args) throws IOException {
-        int curByte, prevByte;
-        prevByte = System.in.read();
-        while (prevByte >= 0) {
-            curByte = System.in.read();
-            if (prevByte != CAR_RET || curByte != NEW_LINE)
-                System.out.write(prevByte);
-            prevByte = curByte;
+        byte[] in_buf = new byte[1024];
+        byte[] out_buf = new byte[1025];
+        int read_bytes = 0;
+
+        while ((read_bytes = System.in.read(in_buf, 0, in_buf.length)) > 0)
+        {
+            int len = convertToUnixLineEnd(in_buf, read_bytes, out_buf);
+            System.out.write(out_buf, 0, len);
         }
+
         System.out.flush();
+    }
+
+    private static int convertToUnixLineEnd(byte[] in_arr, int length, byte[] out_arr) {
+        assert(in_arr.length <= out_arr.length);
+        int j = 0;
+
+        if (last_cr && in_arr[0] != LF) {
+            out_arr[j++] = CR;
+        }
+
+        last_cr = false;
+
+        for (int i = 0; i < length; ++i) {
+            if (in_arr[i] != CR || (i + 1 != length && in_arr[i + 1] != LF)) {
+                out_arr[j++] = in_arr[i];
+            }
+            else
+            {
+                if (i + 1 == in_arr.length)
+                {
+                    last_cr = true;
+                }
+            }
+        }
+
+        return j;
     }
 }
