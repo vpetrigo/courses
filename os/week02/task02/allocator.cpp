92551c8e1d982b0924b4268d3318514169555c67
Vladimir Petrigo <taenaru@gmail.com>
2018-02-04 18:56:56 +0300

    os: Update free routine for Allocator (w02 t02)

--- os/week02/task02/allocator.cpp
+++ os/week02/task02/allocator.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <stdexcept>
 
@@ -7,77 +8,79 @@
 #define log_debug(...)
 #endif
 
-class Allocator
-{
-public:
-    struct BorderMarker
-	{
-        BorderMarker(BorderMarker *prev, BorderMarker *next, std::size_t size) : prev_{prev}, next_{next}, size_{size}, is_free_{BLOCK_FREE} {}
+class Allocator {
+  public:
+    struct BorderMarker {
+        BorderMarker(BorderMarker *prev, BorderMarker *next, std::size_t size)
+            : prev_{prev}, next_{next}, size_{size}, is_free_{BLOCK_FREE}
+        {
+        }
 
-		BorderMarker *prev_;
-		BorderMarker *next_;
-		std::size_t size_;
+        BorderMarker *prev_;
+        BorderMarker *next_;
+        std::size_t size_;
         char is_free_;
-	};
+    };
 
     Allocator() : head_{nullptr}, size_{0} {}
-	Allocator(void *buf, std::size_t size) : head_{buf}, start_{head_}, size_{size}
+    Allocator(void *buf, std::size_t size)
+        : head_{buf}, start_{head_}, size_{size}
     {
         size_ -= 2 * sizeof(BorderMarker);
-        new(head_) BorderMarker{nullptr, nullptr, size_};
-        new(get_ptr_to_end_bm(head_, size_)) BorderMarker{nullptr, nullptr, size_};
+        new (head_) BorderMarker{nullptr, nullptr, size_};
+        new (get_ptr_to_end_bm(head_, size_))
+            BorderMarker{nullptr, nullptr, size_};
     }
 
-	~Allocator() = default;
+    ~Allocator() = default;
 
-	Allocator(const Allocator &alloc) = delete;
-	Allocator& operator=(const Allocator &alloc) = delete;
+    Allocator(const Allocator &alloc) = delete;
+    Allocator &operator=(const Allocator &alloc) = delete;
 
     void set_buffer(void *buf, std::size_t size) noexcept
     {
         head_ = start_ = buf;
         size_ = size - 2 * sizeof(BorderMarker);
-        new(head_) BorderMarker{nullptr, nullptr, size_};
-        new(get_ptr_to_end_bm(head_, size_)) BorderMarker{nullptr, nullptr, size_};
+        new (head_) BorderMarker{nullptr, nullptr, size_};
+        new (get_ptr_to_end_bm(head_, size_))
+            BorderMarker{nullptr, nullptr, size_};
     }
 
-    void *alloc(std::size_t size) noexcept
-    {
-        return nullptr;    
-    }
+    void *alloc(std::size_t size) noexcept { return nullptr; }
 
     void free(void *ptr) noexcept
     {
-        BorderMarker *bm = get_bm_by_ptr(ptr); 
-        
-        defrag(bm);
-        
-        BorderMarker *on_head = static_cast<BorderMarker *>(head_);
+        BorderMarker *bm = get_bm_by_ptr(ptr);
+        BorderMarker *free_region = defrag(bm);
+
+        log_debug("%s: free reg: %p, bm: %p\n", __func__, free_region, bm);
     }
-    
+
     const BorderMarker &debug_get_borders(void *ptr) const
     {
-        if (!ptr)
-        {
+        if (!ptr) {
             throw std::logic_error{""};
         }
 
         return *get_bm_by_ptr(ptr);
     }
-private:
+
+  private:
     static constexpr char BLOCK_OCCUPIED = 0;
     static constexpr char BLOCK_FREE = 1;
 
     constexpr void *get_mem_end() const noexcept
     {
-        return static_cast<void *>(static_cast<char *>(start_) + size_ - sizeof(BorderMarker));
+        return static_cast<void *>(static_cast<char *>(start_) + size_ -
+                                   sizeof(BorderMarker));
     }
 
     BorderMarker *get_bm_by_ptr(void *ptr) const
     {
-        return reinterpret_cast<BorderMarker *>(static_cast<char *>(ptr) - sizeof(BorderMarker));
+        return reinterpret_cast<BorderMarker *>(static_cast<char *>(ptr) -
+                                                sizeof(BorderMarker));
     }
-    
+
     char *get_ptr_to_end_bm(void *ptr, std::size_t size)
     {
         return static_cast<char *>(ptr) + sizeof(BorderMarker) + size;
@@ -90,21 +93,25 @@ private:
 
     BorderMarker *defrag(BorderMarker *bm) noexcept
     {
-        BorderMarker *start = defrag_prev(bm);
-        defrag_next(bm);
+        BorderMarker *start1 = defrag_prev(bm);
+        BorderMarker *start2 = defrag_next(bm);
+        assert(start1 == start2);
+
+        return start1;
     }
 
     BorderMarker *defrag_prev(BorderMarker *bm) noexcept
     {
         BorderMarker *prev = bm - 1;
 
-        if (bm != start_ && prev->is_free_ == BLOCK_FREE)
-        {
+        if (bm != start_ && prev->is_free_ == BLOCK_FREE) {
             log_debug("%s: Defragment previous\n", __func__);
-            prev = reinterpret_cast<BorderMarker *>(get_ptr_to_start_bm(prev, prev->size_));
+            prev = reinterpret_cast<BorderMarker *>(
+                get_ptr_to_start_bm(prev, prev->size_));
             prev->size_ += bm->size_ + 2 * sizeof(BorderMarker);
 
-            BorderMarker *end_bm = reinterpret_cast<BorderMarker *>(get_ptr_to_end_bm(bm, bm->size_));
+            BorderMarker *end_bm = reinterpret_cast<BorderMarker *>(
+                get_ptr_to_end_bm(bm, bm->size_));
 
             *end_bm = *prev;
             bm = prev;
@@ -115,22 +122,37 @@ private:
 
     BorderMarker *defrag_next(BorderMarker *bm) noexcept
     {
-        BorderMarker *end_bm = reinterpret_cast<BorderMarker *>(get_ptr_to_end_bm(bm, bm->size_));
+        BorderMarker *end_bm =
+            reinterpret_cast<BorderMarker *>(get_ptr_to_end_bm(bm, bm->size_));
         BorderMarker *next = end_bm + 1;
 
-        if (end_bm != get_mem_end() && next->is_free_ == BLOCK_FREE)
-        {
+        if (end_bm != get_mem_end() && next->is_free_ == BLOCK_FREE) {
             log_debug("%s: Defragment next\n", __func__);
-            if (next->prev_)
-            {
-                next->prev_->size_ += next->size_ + 2 * sizeof(BorderMarker);
-                next->prev_->next_ = next->next_;
 
-                BorderMarker *next_end = reinterpret_cast<BorderMarker *>(get_ptr_to_end_bm(next, next->size_));
+            BorderMarker *next_end = reinterpret_cast<BorderMarker *>(
+                get_ptr_to_end_bm(next, next->size_));
 
-                *next_end = *next->prev_;
-                bm = next->prev_;
+            next_end->size_ += end_bm->size_ + 2 * sizeof(BorderMarker);
+            next_end->prev_ = next->prev_;
+            next_end->next_ = next->next_;
+
+            BorderMarker *region_start = reinterpret_cast<BorderMarker *>(
+                get_ptr_to_start_bm(next_end + 1, next_end->size_));
+            *region_start = *next_end;
+
+            if (next->next_) {
+                next->next_->prev_ = region_start;
             }
+
+            if (next->prev_) {
+                next->prev_->next_ = region_start;
+            }
+
+            if (next == head_) {
+                head_ = region_start;
+            }
+
+            bm = region_start;
         }
 
         return bm;
@@ -139,7 +161,7 @@ private:
     // data members
     void *head_;
     void *start_;
-	std::size_t size_;
+    std::size_t size_;
 };
 
 static Allocator allocator;
@@ -151,28 +173,19 @@ static Allocator allocator;
 //       должен распределять, все возвращаемые указатели должны быть
 //       либо равны NULL, либо быть из этого участка памяти
 // size - размер участка памяти, на который указывает buf
-void mysetup(void *buf, std::size_t size)
-{
-    allocator.set_buffer(buf, size);
-}
+void mysetup(void *buf, std::size_t size) { allocator.set_buffer(buf, size); }
 
 // Функция аллокации
-void *myalloc(std::size_t size)
-{
-    return allocator.alloc(size);
-}
+void *myalloc(std::size_t size) { return allocator.alloc(size); }
 
 // Функция освобождения
-void myfree(void *p)
-{
-    allocator.free(p);
-}
+void myfree(void *p) { allocator.free(p); }
 
 int main()
 {
     constexpr std::size_t mem_size = 102400;
     char *mem = new char[mem_size];
-    
+
     mysetup(mem, mem_size);
 
     delete[] mem;
