ab8d0ffb40a2c1d69b2151eee9edd8a24fe054f3
Vladimir Petrigo <taenaru@gmail.com>
2018-02-16 07:52:22 +0300

    os: Update allocator implementation for week02 task02

--- os/week02/task02/allocator.hpp
+++ os/week02/task02/allocator.hpp
@@ -15,12 +15,12 @@ constexpr char *to_char(void *ptr)
 class Allocator
 {
   public:
-    Allocator() noexcept : head_{nullptr}, end_{nullptr}
+    Allocator() noexcept : head_{nullptr}, start_{nullptr}, end_{nullptr}
     {
     }
 
     Allocator(void *buf, std::size_t size) noexcept
-        : head_{buf}, end_{to_char(head_) + size}
+        : head_{buf}, start_{head_}, end_{to_char(head_) + size}
     {
         new (head_)
             BorderMarker{nullptr, nullptr, size - 2 * sizeof(BorderMarker)};
@@ -90,6 +90,51 @@ class Allocator
         return alloc_place.first + 1;
     }
 
+    void free(void *ptr) noexcept
+    {
+        if (!ptr)
+        {
+            return;
+        }
+
+        BorderMarker *user_block_start = get_bm_by_user_ptr(ptr);
+        BorderMarker *user_block_end = get_bm_by_ptr_with_offset(
+            user_block_start + 1, user_block_start->size_);
+
+        if (is_left_block_exists(user_block_start) &&
+            is_left_block_free(user_block_start))
+        {
+            // defrag left
+            user_block_start = defrag_left(user_block_start, user_block_end);
+        }
+
+        if (is_right_block_exists(user_block_end + 1) &&
+            is_right_block_free(user_block_end))
+        {
+            // defrag right
+            if (user_block_end + 1 == head_)
+            {
+                head_ = user_block_start;
+            }
+
+            user_block_end = defrag_right(user_block_start, user_block_end);
+        }
+
+        if (head_ == end_)
+        {
+            head_ = user_block_start;
+        }
+
+        user_block_start->is_free_ = MemOccupation::BLOCK_FREE;
+        user_block_end->is_free_ = MemOccupation::BLOCK_FREE;
+
+        if (user_block_start->prev_ == nullptr &&
+            user_block_start->next_ == nullptr && user_block_start != head_)
+        {
+            add_mem_block_to_list(user_block_start);
+        }
+    }
+
     static constexpr std::size_t get_allocation_overhead() noexcept
     {
         return 2 * sizeof(BorderMarker);
@@ -147,7 +192,8 @@ class Allocator
         return static_cast<BorderMarker *>(get_offset_to_bm(ptr));
     }
 
-    BorderMarker *get_bm_by_ptr_with_offset(void *ptr, std::size_t offset) const
+    BorderMarker *get_bm_by_ptr_with_offset(void *ptr,
+                                            std::ptrdiff_t offset) const
         noexcept
     {
         return reinterpret_cast<BorderMarker *>(to_char(ptr) + offset);
@@ -167,16 +213,9 @@ class Allocator
     void occupy_block(BorderMarker *bm) const noexcept
     {
         bm->is_free_ = MemOccupation::BLOCK_OCCUPIED;
-
-        if (bm->prev_)
-        {
-            bm->prev_->next_ = bm->next_;
-        }
-
-        if (bm->next_)
-        {
-            bm->next_->prev_ = bm->prev_;
-        }
+        delete_from_list(bm);
+        bm->prev_ = nullptr;
+        bm->next_ = nullptr;
     }
 
     std::pair<BorderMarker *, bool> check_free_mem(std::size_t size) const
@@ -208,7 +247,98 @@ class Allocator
         return bm->size_ - to_alloc > Allocator::get_allocation_overhead();
     }
 
+    bool is_left_block_exists(void *ptr) const noexcept
+    {
+        return ptr != start_;
+    }
+
+    bool is_right_block_exists(void *ptr) const noexcept
+    {
+        return ptr != end_;
+    }
+
+    bool is_left_block_free(const BorderMarker *bm) const noexcept
+    {
+        return (bm - 1)->is_free_ == MemOccupation::BLOCK_FREE;
+    }
+
+    bool is_right_block_free(const BorderMarker *bm) const noexcept
+    {
+        return (bm + 1)->is_free_ == MemOccupation::BLOCK_FREE;
+    }
+
+    BorderMarker *defrag_left(BorderMarker *start_bm,
+                              BorderMarker *end_bm) const noexcept
+    {
+        BorderMarker *left_end_bm = start_bm - 1;
+        BorderMarker *left_bm =
+            get_bm_by_ptr_with_offset(left_end_bm - 1, -left_end_bm->size_);
+
+        left_bm->size_ +=
+            start_bm->size_ + Allocator::get_allocation_overhead();
+        *end_bm = *left_bm;
+
+        return left_bm;
+    }
+
+    BorderMarker *defrag_right(BorderMarker *start_bm,
+                               BorderMarker *end_bm) const noexcept
+    {
+        BorderMarker *right_bm = end_bm + 1;
+        BorderMarker *right_end_bm =
+            get_bm_by_ptr_with_offset(right_bm + 1, right_bm->size_);
+
+        start_bm->size_ +=
+            right_bm->size_ + Allocator::get_allocation_overhead();
+        // remove start_bm from the list
+        delete_from_list(start_bm);
+
+        if (right_bm->next_)
+        {
+            right_bm->next_->prev_ = start_bm;
+        }
+
+        if (right_bm->prev_)
+        {
+            right_bm->prev_->next_ = start_bm;
+        }
+
+        start_bm->next_ = right_bm->next_;
+        start_bm->prev_ = right_bm->prev_;
+        *right_end_bm = *start_bm;
+
+        return right_end_bm;
+    }
+
+    void add_mem_block_to_list(BorderMarker *bm) noexcept
+    {
+        BorderMarker *it = get_bm_by_ptr(head_);
+
+        while (it->next_)
+        {
+            it = it->next_;
+        }
+
+        it->next_ = bm;
+        bm->next_ = nullptr;
+        bm->prev_ = it;
+    }
+
+    void delete_from_list(BorderMarker *bm) const noexcept
+    {
+        if (bm->next_)
+        {
+            bm->next_->prev_ = bm->prev_;
+        }
+
+        if (bm->prev_)
+        {
+            bm->prev_->next_ = bm->next_;
+        }
+    }
+
   private:
     void *head_;
+    void *start_;
     void *end_;
 };
