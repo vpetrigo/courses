88cf6c759ca7b2250efaa9cf20ac6fb682da8f33
Vladimir Petrigo <taenaru@gmail.com>
2018-04-11 19:57:12 +0300

    os: Switch cache to array slab task03

--- os/week02/task03/cache_alloc.cpp
+++ os/week02/task03/cache_alloc.cpp
@@ -363,7 +363,7 @@ struct cache {
                 void *new_mem = alloc_slab(slab_order);
                 array_slab *new_slab =
                     new (static_cast<char *>(new_mem) + alloc_size(slab_order) -
-                         sizeof(slab)) slab{new_mem, slab_order, object_size};
+                         sizeof(array_slab)) array_slab{new_mem, slab_order, object_size, slab_max_elems};
                 list_append(&slabs_partial, &new_slab->slabs);
                 sl = new_slab;
             }
