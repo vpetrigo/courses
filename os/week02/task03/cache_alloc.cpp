f6e9a42d003c47e348408f32d86ef0e17b1cccf2
Vladimir Petrigo <taenaru@gmail.com>
2018-05-19 20:18:25 +0300

    os: Clean up code formatting for task03

--- os/week02/task03/cache_alloc.cpp
+++ os/week02/task03/cache_alloc.cpp
@@ -587,7 +587,7 @@ void test2()
     cache_free(&mem_cache, data1);
     cache_free(&mem_cache, data2);
     cache_shrink(&mem_cache);
-    //cache_release(&mem_cache);
+    // cache_release(&mem_cache);
     std::cout << __func__ << ": passed" << std::endl;
 }
 
