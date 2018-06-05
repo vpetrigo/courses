4531b0daa13de9e6a5350a914fa14099e45259da
Vladimir Petrigo <taenaru@gmail.com>
2018-06-05 21:08:44 +0300

    os: Add CMakeLists.txt for task01 week03

---
+++ os/week03/task01/simple_scheduler.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+int main()
+{
+    using Task = std::pair<int, int>;
+    std::vector<Task> tasks;
+    int id = 0;
+    int n;
+    std::cin >> n;
+
+    tasks.reserve(n);
+    std::istream_iterator<int> it{std::cin};
+    std::istream_iterator<int> sent{};
+    std::transform(it, sent, std::back_inserter(tasks), [&id](const int &elem) {
+        return std::make_pair(elem, id++);
+    });
+    std::sort(tasks.begin(), tasks.end());
+    std::for_each(tasks.cbegin(), tasks.cend(),
+                  [](const auto &task) { std::cout << task.second << " "; });
+
+    return 0;
+}
