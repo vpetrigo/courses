7f26a9e95f6944e7b64ba7f7be0429af28c4cd7a
Vladimir Petrigo <taenaru@gmail.com>
2018-06-11 17:47:31 +0300

    os: Clean up

--- os/week03/task01/simple_scheduler.py
+++ os/week03/task01/simple_scheduler.py
@@ -1,5 +1,5 @@
 n = input()
 
-print(' '.join(map(lambda t: str(t[0]),
+print(" ".join(map(lambda t: str(t[0]),
                sorted(enumerate(map(int, input().split())),
                       key=lambda t: t[1]))))
