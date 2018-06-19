4622f156e0861278e9fac658f87a253d18baff1d
Vladimir Petrigo <taenaru@gmail.com>
2018-06-19 18:51:20 +0300

    kotlin: Add solution for task04 week01

---
+++ kotlin/week01/task04/defaultArgs.kt
@@ -0,0 +1,15 @@
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+fun main(args: Array<String>) {
+    for (elem in useFoo()) {
+        println(elem)
+    }
+}
