eacdb563f000510b91ea0489c25d657f3eb02a18
Vladimir Petrigo <taenaru@gmail.com>
2018-07-12 10:00:42 +0300

    kotlin: Update build.gradle to JUnit5 support

--- kotlin/week05/task02/build.gradle.kts
+++ kotlin/week05/task02/build.gradle.kts
@@ -10,4 +10,11 @@ application {
 dependencies {
     compile(kotlin("stdlib"))
     testCompile("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.2.0")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.2.0")
 }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
