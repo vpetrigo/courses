1e8d34c0871b500c668fb7f0011bcf2dbfa7879c
Vladimir Petrigo <taenaru@gmail.com>
2018-07-15 04:12:13 +0300

    adaptive-java: Clean up the task56 section03

--- programming/adaptive-java/section03/task56/src/main/java/ProductCollector.java
+++ programming/adaptive-java/section03/task56/src/main/java/ProductCollector.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class ProductCollector {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(2, 3, 4);
-        long val = numbers.stream().collect(Collectors.reducing(1, (a, b) -> a * a * b * b));
+        long val = numbers.stream().collect(Collectors.reducing(1, (a, b) -> a * b * b));
 
         System.out.println(val);
     }
