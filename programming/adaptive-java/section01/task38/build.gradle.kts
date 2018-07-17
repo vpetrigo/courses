a1027ac13fb0f66de7ffbc22e6834eb1f6a6c86a
Vladimir Petrigo <taenaru@gmail.com>
2018-07-17 21:59:52 +0300

    adaptive-java: Play around with JetBrains annotations

--- programming/adaptive-java/section01/task38/build.gradle.kts
+++ programming/adaptive-java/section01/task38/build.gradle.kts
@@ -7,6 +7,7 @@ application {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:16.0.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
 }
--- programming/adaptive-java/section01/task38/src/main/java/section01/task38/GradeCounter.java
+++ programming/adaptive-java/section01/task38/src/main/java/section01/task38/GradeCounter.java
@@ -1,10 +1,12 @@
 package section01.task38;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class GradeCounter {
-    public static int[] calculateGradesDistribution(List<Integer> grades) {
+    public static int[] calculateGradesDistribution(@NotNull List<Integer> grades) {
         int[] gradesDistr = new int[4];
         grades.forEach((elem) -> gradesDistr[elem - 2] += 1);
 
@@ -18,5 +20,6 @@ public class GradeCounter {
 
         IntStream.range(0, nGrades).forEach((n) -> grades.add(reader.nextInt()));
         Arrays.stream(calculateGradesDistribution(grades)).forEach((e) -> System.out.printf("%d ", e));
+        calculateGradesDistribution(null);
     }
 }
