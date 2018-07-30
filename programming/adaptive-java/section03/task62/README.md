a2ec1b309d4e4f84cdfdb16103da12169a0e5406
Vladimir Petrigo <taenaru@gmail.com>
2018-07-30 14:26:18 +0300

    adaptive-java: Update solution for the task62 section03

--- programming/adaptive-java/section03/task62/README.md
+++ programming/adaptive-java/section03/task62/README.md
@@ -1,3 +1,5 @@
+## Task 1
+
 Write a curried form of the function `f(x,y,z)=x+y∗y+z∗z∗z` using lambda expressions
 in Java 8 style. The result and `x`, `y`, `z` must be integer numbers.
 
@@ -27,4 +29,40 @@ An example of a curried function: `x -> y -> ...`;
 
 ```
 75
+```
+
+## Task 2
+
+Write a curried function (using lambdas) that accepts four string
+arguments and concatenated all in one string following the rules:
+
+String cases: in the result string, first and second arguments must
+be in lower cases and third and fourth in upper cases.
+Order of arguments concatenation: first, third, second, fourth.
+Solution format. You may write the result in any valid formats but with ; on the end.
+
+An example of a curried function: `x -> y -> ...`;
+
+### Sample Input 1:
+
+```
+aa bb cc dd
+```
+
+### Sample Output 1:
+
+```
+aaCCbbDD
+```
+
+### Sample Input 2:
+
+```
+AAA bbb CCC ddd
+```
+
+### Sample Output 2:
+
+```
+aaaCCCbbbDDD
 ```
--- programming/adaptive-java/section03/task62/src/main/java/section03/task62/Main.java
+++ programming/adaptive-java/section03/task62/src/main/java/section03/task62/Main.java
@@ -3,9 +3,21 @@ package section03.task62;
 import java.util.function.Function;
 
 public class Main {
+    public static Function<Long, Function<Long, Function<Long, Long>>> getMathFun() {
+        return x -> y -> z -> x + y * y + z * z * z;
+    }
+
+    public static Function<String, Function<String, Function<String, Function<String, String>>>> getStringFun() {
+        return a -> b -> c -> d -> a.toLowerCase() + c.toUpperCase() + b.toLowerCase() + d.toUpperCase();
+    }
+
     public static void main(String[] args) {
-        Function<Long, Function<Long, Function<Long, Long>>> fun = x -> y -> z -> x + y * y + z * z * z;
+        Function<Long, Function<Long, Function<Long, Long>>> fun = getMathFun();
 
         System.out.println(fun.apply(1L).apply(2L).apply(3L));
+
+        Function<String, Function<String, Function<String, Function<String, String>>>> strFun = getStringFun();
+
+        System.out.println(strFun.apply("aa").apply("bb").apply("cc").apply("dd"));
     }
 }
