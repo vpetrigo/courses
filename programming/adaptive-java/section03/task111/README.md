2b296d836142cb964cf62dac0827bb34b3c0ab16
Vladimir Petrigo <taenaru@gmail.com>
2018-08-23 17:59:43 +0300

    adaptive-java: Add solution for the task111 section03

---
+++ programming/adaptive-java/section03/task111/README.md
@@ -0,0 +1,12 @@
+Create `ArrayDeque` by name queue in any way known to you and push
+the following four numbers `2`, `0`, `1`, `7`.
+
+The code for displaying the queue is already written.
+
+### Sample Input:
+
+### Sample Output:
+
+```
+[2, 0, 1, 7]
+```
---
+++ programming/adaptive-java/section03/task111/src/main/java/section03/task111/Main.java
@@ -0,0 +1,11 @@
+package section03.task111;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>(Arrays.asList(2, 0, 1, 7));
+        System.out.println(queue);
+    }
+}
