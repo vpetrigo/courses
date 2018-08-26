8cb0981f34f7d05ae5665bb78c121850b6f502d1
Vladimir Petrigo <taenaru@gmail.com>
2018-08-26 20:33:15 +0300

    adaptive-java: Add tests for the task50 section03

--- programming/adaptive-java/section03/task50/build.gradle.kts
+++ programming/adaptive-java/section03/task50/build.gradle.kts
@@ -5,3 +5,12 @@ plugins {
 application {
     mainClassName = "section03.task50.Main"
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ programming/adaptive-java/section03/task50/src/test/java/section03/task50/MainTest.java
@@ -0,0 +1,19 @@
+package section03.task50;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainTest {
+    @Test
+    void testFilter() {
+        IntStream oddStream = IntStream.builder().add(75).build();
+        IntStream evenStream = IntStream.builder().add(30).add(60).add(90).build();
+        IntStream concatStream = Main.createFilteringStream(oddStream, evenStream);
+
+        Assertions.assertEquals(Arrays.asList(75, 90), concatStream.boxed().collect(Collectors.toList()));
+    }
+}
