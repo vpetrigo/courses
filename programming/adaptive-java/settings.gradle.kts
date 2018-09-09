a9aee7f48173f19714b1fadf3694bf7e85dc24b4
Vladimir Petrigo <taenaru@gmail.com>
2018-09-09 17:58:06 +0300

    adaptive-java: Update settings.gradle.kts

--- programming/adaptive-java/settings.gradle.kts
+++ programming/adaptive-java/settings.gradle.kts
@@ -21,4 +21,4 @@ include(
     ":section01:task140", ":section01:task154", ":section01:task06", ":section01:task39",
     ":section01:task40", ":section01:task01", ":section01:task12", ":section01:task19",
     ":section01:task126", ":section01:task26", ":section01:task58", ":section03:task111",
-    ":section03:task50")
+    ":section03:task50", ":section01:task84", ":section03:task63", ":section01:task167")
