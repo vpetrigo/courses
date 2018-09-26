390005d60bf70b2fe60cf5fab2f9dd73165575a2
Vladimir Petrigo <taenaru@gmail.com>
2018-09-27 01:32:16 +0300

    adaptive-java: Fix the task123 section01 to work with JDK11

--- programming/adaptive-java/section01/task123/build.gradle.kts
+++ programming/adaptive-java/section01/task123/build.gradle.kts
@@ -6,6 +6,24 @@ application {
     mainClassName = "section01.task123.MatrixMax"
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+var compileSystem = org.gradle.internal.os.OperatingSystem.current()!!
+var platform: String = ""
+
+when {
+    compileSystem.isWindows -> platform = "win"
+    compileSystem.isLinux -> platform = "linux"
+    compileSystem.isMacOsX -> platform = "mac"
+}
+
+dependencies {
+    implementation("org.openjfx:javafx-base:11:$platform")
+}
+
 tasks.withType<JavaExec> {
     standardInput = System.`in`
 }
