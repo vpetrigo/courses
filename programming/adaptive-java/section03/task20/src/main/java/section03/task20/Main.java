dbfe38d6b21156cc7a47a5ae2976819d14ae132f
Vladimir Petrigo <taenaru@gmail.com>
2018-10-18 19:24:11 +0300

    adaptive-java: Update the task20 section03 with new solutions

--- programming/adaptive-java/section03/task20/src/main/java/section03/task20/Main.java
+++ programming/adaptive-java/section03/task20/src/main/java/section03/task20/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,6 +13,12 @@ public class Main {
                 new BufferedReader(new InputStreamReader(System.in));
         List<String> list = Arrays.asList(reader.readLine().split("\\s"));
 
+        solution1(list);
+        solution2(list);
+        solution3(list);
+    }
+
+    public static void solution1(List<String> list) {
         list.forEach(s -> {
             System.out.println(s);
 
@@ -19,5 +26,32 @@ public class Main {
                 System.out.println("Hop");
             }
         });
+    }
+
+    public static void solution2(List<String> list) {
+        ListIterator<String> listIterator = list.listIterator();
+
+        while (listIterator.hasNext())
+        {
+            String currentString = listIterator.next();
+            System.out.println(currentString);
+
+            if (currentString.equals("Hip"))
+            {
+                System.out.println("Hop");
+            }
+        }
+    }
+
+    public static void solution3(List<String> list) {
+        for (String string : list)
+        {
+            System.out.println(string);
+
+            if (string.equals("Hip"))
+            {
+                System.out.println("Hop");
+            }
+        }
     }
 }
