cd55143c421802cde9de61acc5fe2ec563a524db
Vladimir Petrigo <taenaru@gmail.com>
2018-10-20 20:40:20 +0300

    adaptive-java: Add build.gradle for the task14 section03

---
+++ programming/adaptive-java/section03/task16/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    application
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClassName = "section03.task16.Main"
+}
