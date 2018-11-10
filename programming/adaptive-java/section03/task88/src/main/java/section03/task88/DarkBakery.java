40f5f1360a55351804e459ceff6979f795a0a3e9
Vladimir Petrigo <taenaru@gmail.com>
2018-11-11 01:52:57 +0300

    adaptive-java: Clean up

---
+++ programming/adaptive-java/section03/task88/src/main/java/section03/task88/DarkBakery.java
@@ -0,0 +1,10 @@
+package section03.task88;
+
+public class DarkBakery extends Bakery {
+    private String greeting;
+
+    public DarkBakery(String greeting) {
+        super();
+        this.greeting = greeting;
+    }
+}
