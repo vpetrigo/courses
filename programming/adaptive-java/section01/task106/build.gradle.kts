c039949c549dea167672288aa008f79b5c85b613
Vladimir Petrigo <taenaru@gmail.com>
2019-01-03 11:53:20 +0300

    adaptive-java: Add build.gradle.kts for the task106 section01

---
+++ programming/adaptive-java/section01/task106/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    application
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClassName = "section01.task106.Main"
+}
