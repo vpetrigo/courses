bec5c4fa714c222d4f7ecfdf126bc6d62a4b1d61
Vladimir Petrigo <taenaru@gmail.com>
2019-05-02 03:20:32 +0300

    alg: Fix check_tree tests

--- alg-datastruct/week4/tree_traverse_test.cpp
+++ alg-datastruct/week4/tree_traverse_test.cpp
@@ -7,7 +7,7 @@
 TEST(TreeTraverse_Test, in_order)
 {
   const std::vector<TreeNode> tree{
-      {4, 1, 2}, {2, 3, 4}, {5, -1, -1}, {1, -1, -1}, {3, -1, -1}};
+      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
   TreeTraverse ttrv{&tree};
   const std::vector<std::size_t> right_order{1, 2, 3, 4, 5};
 
@@ -18,7 +18,7 @@ TEST(TreeTraverse_Test, in_order)
 TEST(TreeTraverse_Test, pre_order)
 {
   const std::vector<TreeNode> tree{
-      {4, 1, 2}, {2, 3, 4}, {5, -1, -1}, {1, -1, -1}, {3, -1, -1}};
+      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
   TreeTraverse ttrv{&tree};
   const std::vector<std::size_t> right_order{4, 2, 1, 3, 5};
 
@@ -29,7 +29,7 @@ TEST(TreeTraverse_Test, pre_order)
 TEST(TreeTraverse_Test, post_order)
 {
   const std::vector<TreeNode> tree{
-      {4, 1, 2}, {2, 3, 4}, {5, -1, -1}, {1, -1, -1}, {3, -1, -1}};
+      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
   TreeTraverse ttrv{&tree};
   const std::vector<std::size_t> right_order{1, 3, 2, 5, 4};
 
