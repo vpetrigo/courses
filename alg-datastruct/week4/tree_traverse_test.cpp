374fafae79ce38b1226734bd7aab4d25e6d34f88
Vladimir Petrigo <taenaru@gmail.com>
2019-05-03 09:43:32 +0300

    misc: Clean up

--- alg-datastruct/week4/tree_traverse_test.cpp
+++ alg-datastruct/week4/tree_traverse_test.cpp
@@ -1,38 +1,44 @@
 #include "tree_traverse.hpp"
 
+#include <algorithm>
 #include <gtest/gtest.h>
 #include <iostream>
-#include <algorithm>
 
 TEST(TreeTraverse_Test, in_order)
 {
-  const std::vector<TreeNode> tree{
-      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
-  TreeTraverse ttrv{&tree};
-  const std::vector<std::size_t> right_order{1, 2, 3, 4, 5};
+    const std::vector<TreeNode> tree{TreeNode{4, 1, 2}, TreeNode{2, 3, 4},
+                                     TreeNode{5, -1, -1}, TreeNode{1, -1, -1},
+                                     TreeNode{3, -1, -1}};
+    TreeTraverse ttrv{&tree};
+    const std::vector<std::size_t> right_order{1, 2, 3, 4, 5};
 
-  ttrv.InOrder(tree[0]);
-  ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(), ttrv.GetOrder().cbegin()));
+    ttrv.InOrder(tree[0]);
+    ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(),
+                           ttrv.GetOrder().cbegin()));
 }
 
 TEST(TreeTraverse_Test, pre_order)
 {
-  const std::vector<TreeNode> tree{
-      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
-  TreeTraverse ttrv{&tree};
-  const std::vector<std::size_t> right_order{4, 2, 1, 3, 5};
+    const std::vector<TreeNode> tree{TreeNode{4, 1, 2}, TreeNode{2, 3, 4},
+                                     TreeNode{5, -1, -1}, TreeNode{1, -1, -1},
+                                     TreeNode{3, -1, -1}};
+    TreeTraverse ttrv{&tree};
+    const std::vector<std::size_t> right_order{4, 2, 1, 3, 5};
 
-  ttrv.PreOrder(tree[0]);
-  ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(), ttrv.GetOrder().cbegin()));
+    ttrv.PreOrder(tree[0]);
+    ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(),
+                           ttrv.GetOrder().cbegin()));
 }
 
 TEST(TreeTraverse_Test, post_order)
 {
-  const std::vector<TreeNode> tree{
-      TreeNode{4, 1, 2}, TreeNode{2, 3, 4}, TreeNode{5, -1, -1}, TreeNode{1, -1, -1}, TreeNode{3, -1, -1}};
-  TreeTraverse ttrv{&tree};
-  const std::vector<std::size_t> right_order{1, 3, 2, 5, 4};
+    const std::vector<TreeNode> tree{TreeNode{4, 1, 2}, TreeNode{2, 3, 4},
+                                     TreeNode{5, -1, -1}, TreeNode{1, -1, -1},
+                                     TreeNode{3, -1, -1}};
+    TreeTraverse ttrv{&tree};
+    const std::vector<std::size_t> right_order{1, 3, 2, 5, 4};
 
-  ttrv.PostOrder(tree[0]);
-  ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(), ttrv.GetOrder().cbegin()));
+    ttrv.PostOrder(tree[0]);
+    ASSERT_TRUE(std::equal(right_order.cbegin(), right_order.cend(),
+                           ttrv.GetOrder().cbegin()));
 }
