e34350e5d9c9afe4caee0667f2bed40eedfc2e41
Vladimir Petrigo <taenaru@gmail.com>
2019-05-04 10:21:26 +0300

    misc: Clean up

--- alg-datastruct/week4/tree_traverse.cpp
+++ alg-datastruct/week4/tree_traverse.cpp
@@ -5,35 +5,35 @@
 
 int main()
 {
-  std::vector<TreeNode> tree;
-  std::size_t n_nodes;
+    std::vector<TreeNode> tree;
+    std::size_t n_nodes;
 
-  std::cin >> n_nodes;
+    std::cin >> n_nodes;
 
-  while (n_nodes-- > 0) {
-    TreeNode node;
+    while (n_nodes-- > 0) {
+        TreeNode node;
 
-    std::cin >> std::get<0>(node) >> std::get<1>(node) >> std::get<2>(node);
+        std::cin >> std::get<0>(node) >> std::get<1>(node) >> std::get<2>(node);
 
-    tree.emplace_back(std::move(node));
-  }
+        tree.emplace_back(std::move(node));
+    }
 
-  TreeTraverse traverse{&tree};
-  const auto& order = traverse.GetOrder();
+    TreeTraverse traverse{&tree};
+    const auto &order = traverse.GetOrder();
 
-  traverse.InOrder(tree.front());
-  std::for_each(order.cbegin(), order.cend(),
-                [](const std::size_t el) { std::cout << el << ' '; });
-  std::cout << std::endl;
-  traverse.CleanOrder();
-  traverse.PreOrder(tree.front());
-  std::for_each(order.cbegin(), order.cend(),
-                [](const std::size_t el) { std::cout << el << ' '; });
-  std::cout << std::endl;
-  traverse.CleanOrder();
-  traverse.PostOrder(tree.front());
-  std::for_each(order.cbegin(), order.cend(),
-                [](const std::size_t el) { std::cout << el << ' '; });
-  std::cout << std::endl;
-  traverse.CleanOrder();
+    traverse.InOrder(tree.front());
+    std::for_each(order.cbegin(), order.cend(),
+                  [](const std::size_t el) { std::cout << el << ' '; });
+    std::cout << std::endl;
+    traverse.CleanOrder();
+    traverse.PreOrder(tree.front());
+    std::for_each(order.cbegin(), order.cend(),
+                  [](const std::size_t el) { std::cout << el << ' '; });
+    std::cout << std::endl;
+    traverse.CleanOrder();
+    traverse.PostOrder(tree.front());
+    std::for_each(order.cbegin(), order.cend(),
+                  [](const std::size_t el) { std::cout << el << ' '; });
+    std::cout << std::endl;
+    traverse.CleanOrder();
 }
