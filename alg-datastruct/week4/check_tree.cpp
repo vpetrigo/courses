3e8e543181e8e90597d2080a13db565526fa5a8a
Vladimir Petrigo <taenaru@gmail.com>
2019-05-07 07:27:58 +0300

    misc: Clean up

--- alg-datastruct/week4/check_tree.cpp
+++ alg-datastruct/week4/check_tree.cpp
@@ -3,34 +3,36 @@
 #include <algorithm>
 #include <iostream>
 
-int main() {
-  std::size_t n_nodes;
-  std::vector<TreeNode> tree;
+int main()
+{
+    std::size_t n_nodes;
+    std::vector<TreeNode> tree;
 
-  std::cin >> n_nodes;
+    std::cin >> n_nodes;
 
-  for (std::size_t i = 0; i < n_nodes; ++i) {
-    TreeNode node;
+    for (std::size_t i = 0; i < n_nodes; ++i) {
+        TreeNode node;
 
-    std::cin >> std::get<0>(node) >> std::get<1>(node) >> std::get<2>(node);
-    tree.emplace_back(std::move(node));
-  }
-
-  if (n_nodes == 0) {
-    std::cout << "CORRECT" << std::endl;
-  }
-  else {
-    TreeTraverse traverse{&tree};
-
-    traverse.InOrder(tree.front());
-    const auto order = traverse.GetOrder();
-
-    if (std::is_sorted(order.cbegin(), order.cend())) {
-      std::cout << "CORRECT" << std::endl;
-    } else {
-      std::cout << "INCORRECT" << std::endl;
+        std::cin >> std::get<0>(node) >> std::get<1>(node) >> std::get<2>(node);
+        tree.emplace_back(std::move(node));
     }
-  }
 
-  return 0;
+    if (n_nodes == 0) {
+        std::cout << "CORRECT" << std::endl;
+    }
+    else {
+        TreeTraverse traverse{&tree};
+
+        traverse.InOrder(tree.front());
+        const auto order = traverse.GetOrder();
+
+        if (std::is_sorted(order.cbegin(), order.cend())) {
+            std::cout << "CORRECT" << std::endl;
+        }
+        else {
+            std::cout << "INCORRECT" << std::endl;
+        }
+    }
+
+    return 0;
 }
