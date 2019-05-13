32ddd756bb8dcb3b18bad8e3c52e8cbc40efd3d6
Vladimir Petrigo <taenaru@gmail.com>
2019-05-13 05:20:40 +0300

    alg: Clean up week3 code

--- alg-datastruct/week3/rabin_karp.cpp
+++ alg-datastruct/week3/rabin_karp.cpp
@@ -5,18 +5,18 @@
 
 int main()
 {
-  std::string pattern;
-  std::string text;
+    std::string pattern;
+    std::string text;
 
-  std::cin >> pattern >> text;
+    std::cin >> pattern >> text;
 
-  RabinKarp rk{pattern};
-  const auto pattern_occur = rk.GetPatternOccurence(text);
+    RabinKarp rk{pattern};
+    const auto pattern_occur = rk.GetPatternOccurence(text);
 
-  std::for_each(
-      std::cbegin(pattern_occur), std::cend(pattern_occur),
-      [](const std::size_t& occurence) { std::cout << occurence << ' '; });
-  std::cout << std::endl;
+    std::for_each(
+        std::cbegin(pattern_occur), std::cend(pattern_occur),
+        [](const std::size_t &occurence) { std::cout << occurence << ' '; });
+    std::cout << std::endl;
 
-  return 0;
+    return 0;
 }
