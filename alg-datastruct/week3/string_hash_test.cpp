0f13af36616a5eb559de8a8403e5da0548b9dd9e
Vladimir Petrigo <taenaru@gmail.com>
2019-05-18 19:43:49 +0300

    alg: Clean up week3 code

--- alg-datastruct/week3/string_hash_test.cpp
+++ alg-datastruct/week3/string_hash_test.cpp
@@ -2,34 +2,37 @@
 
 #include <gtest/gtest.h>
 
-TEST(StringHashSet, add_delete_one_element) {
-  HashSet<std::string> ht{1};
+TEST(StringHashSet, add_delete_one_element)
+{
+    HashSet<std::string> ht{1};
 
-  ht.Add("Hello");
-  ASSERT_NE(ht.Find("Hello"), ht.End());
+    ht.Add("Hello");
+    ASSERT_NE(ht.Find("Hello"), ht.End());
 
-  auto check_pair = ht.Check(0);
+    auto check_pair = ht.Check(0);
 
-  ASSERT_EQ(*check_pair.first, "Hello");
-  ht.Delete("Hello");
-  ASSERT_EQ(ht.Find("Hello"), ht.End());
+    ASSERT_EQ(*check_pair.first, "Hello");
+    ht.Delete("Hello");
+    ASSERT_EQ(ht.Find("Hello"), ht.End());
 }
 
-TEST(StringHashSet, add_duplicates) {
-  HashSet<std::string> ht{1};
+TEST(StringHashSet, add_duplicates)
+{
+    HashSet<std::string> ht{1};
 
-  ht.Add("Hello");
-  ht.Add("Hello");
-  ht.Add("Hello");
-  auto check_pair = ht.Check(0);
-  ASSERT_EQ(std::distance(check_pair.first, check_pair.second), 1);
-  ht.Add("Test");
-  check_pair = ht.Check(0);
-  ASSERT_EQ(std::distance(check_pair.first, check_pair.second), 2);
+    ht.Add("Hello");
+    ht.Add("Hello");
+    ht.Add("Hello");
+    auto check_pair = ht.Check(0);
+    ASSERT_EQ(std::distance(check_pair.first, check_pair.second), 1);
+    ht.Add("Test");
+    check_pair = ht.Check(0);
+    ASSERT_EQ(std::distance(check_pair.first, check_pair.second), 2);
 }
 
-TEST(StringHashFunction, hash_test) {
-  HashFunction<std::string> hf;
+TEST(StringHashFunction, hash_test)
+{
+    HashFunction<std::string> hf;
 
-  ASSERT_EQ(hf("world") % 5, hf("Hell0") % 5);
+    ASSERT_EQ(hf("world") % 5, hf("Hell0") % 5);
 }
