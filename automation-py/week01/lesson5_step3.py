a6c5a082bbd6302d9f778175eb4d9f38f36e6c0f
Vladimir Petrigo <taenaru@gmail.com>
2019-05-23 02:36:00 +0300

    automation: Add solution for the step3 lession5

---
+++ automation-py/week01/lesson5_step3.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+input1 = browser.find_element_by_tag_name("input")
+input1.send_keys("Ivan")
+input2 = browser.find_element_by_name("last_name")
+input2.send_keys("Petrov")
+input3 = browser.find_element_by_class_name("city")
+input3.send_keys("Smolensk")
+input4 = browser.find_element_by_id("country")
+input4.send_keys("Russia")
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
