5d00a30aad76646dd8d6311648f5a8d03cdfde7c
Vladimir Petrigo <taenaru@gmail.com>
2019-06-23 04:59:57 +0300

    automation: Add test_browser.py

---
+++ automation-py/week03/test_browser.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    return browser
+
+
+class TestMainPage1:
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
