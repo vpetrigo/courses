a4531425eb865bcc318e925c74cc3b6561ec6467
Vladimir Petrigo <taenaru@gmail.com>
2019-06-28 05:28:41 +0300

    automation: Update conftest.py to support Firefox

--- automation-py/week03/step09/conftest.py
+++ automation-py/week03/step09/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 def pytest_addoption(parser):
@@ -8,16 +8,27 @@ def pytest_addoption(parser):
                      action="store",
                      default=None,
                      help="Choose language")
+    parser.addoption("--browser", help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_to_use = request.config.getoption("browser", "chrome")
+    language_to_use = request.config.getoption("language")
     print(
-        f"Run Chrome browser with {request.config.getoption('language')} language"
+        f"Run {browser_to_use} browser with {request.config.getoption('language')} language"
     )
-    options = Options()
-    options.add_argument(f"--lang={request.config.getoption('language')}")
-    browser = webdriver.Chrome(options=options)
+
+    if browser_to_use == "chrome":
+        options = ChromeOptions()
+        options.add_argument(f"--lang={language_to_use}")
+        browser = webdriver.Chrome(options=options)
+    elif browser_to_use == "firefox":
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("intl.accept_languages", language_to_use)
+        browser = webdriver.Firefox(firefox_profile=profile)
+    else:
+        raise NotImplementedError
 
     yield browser
 
