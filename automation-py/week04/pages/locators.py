71264552693af668772c7f9e1b9e91ebd1f8c756
Vladimir Petrigo <taenaru@gmail.com>
2019-07-07 11:28:00 +0300

    automation: Add Login Page locators

--- automation-py/week04/pages/locators.py
+++ automation-py/week04/pages/locators.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators(object):
+class MainPageLocators:
     REGISTRATION_LINK = (By.CSS_SELECTOR, "#registration_link")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
