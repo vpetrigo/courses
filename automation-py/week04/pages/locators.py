80a4d637251d3041e2721e1dabfef722cea6158f
Vladimir Petrigo <taenaru@gmail.com>
2019-07-09 02:54:42 +0300

    automation: Add ProductPageLocators

--- automation-py/week04/pages/locators.py
+++ automation-py/week04/pages/locators.py
@@ -9,3 +9,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner")
+    ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
+    PRICE_VALUE = (By.CLASS_NAME, "price_color")
+    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
