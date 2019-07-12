605f68d90cec4c5e6efd3d6c196dbb6d50652130
Vladimir Petrigo <taenaru@gmail.com>
2019-07-13 02:42:31 +0300

    automation: Update ProductPageLocators to select product name in alert

--- automation-py/week04/pages/locators.py
+++ automation-py/week04/pages/locators.py
@@ -13,7 +13,7 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
-    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner")
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner strong")
     ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
     PRICE_VALUE = (By.CLASS_NAME, "price_color")
     PRODUCT_NAME = (By.TAG_NAME, "h1")
