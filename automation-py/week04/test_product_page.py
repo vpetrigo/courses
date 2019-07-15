9d007516d9ebb895db2c3c81fd26c960587a1f2e
Vladimir Petrigo <taenaru@gmail.com>
2019-07-15 10:30:26 +0300

    automation: Update product page tests to check offers

--- automation-py/week04/test_product_page.py
+++ automation-py/week04/test_product_page.py
@@ -7,10 +7,21 @@ links = [
     "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 ]
 
+offer_link_template = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+offer_links = [f"{offer_link_template}{i}" for i in range(0, 10)]
 
-@pytest.mark.parametrize("product", links)
-def test_guest_can_add_product_to_cart(browser, product: str) -> None:
-    link = product
+
+@pytest.mark.parametrize("link", links)
+def test_guest_can_add_product_to_cart(browser, link: str) -> None:
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_cart(True)
+    product_page.should_be_present_in_cart()
+    product_page.should_check_overall_cost()
+
+
+@pytest.mark.parametrize("link", offer_links)
+def test_guest_can_add_product_to_cart_with_different_offer_numbers(browser, link: str) -> None:
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart(True)
