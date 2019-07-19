a50b308a9f0eacde0a3558c62ce30f3f35759690
Vladimir Petrigo <taenaru@gmail.com>
2019-07-19 06:14:13 +0300

    automation: Update BasePage with is_not_element_present and is_disappeared methods

--- automation-py/week04/pages/base_page.py
+++ automation-py/week04/pages/base_page.py
@@ -1,7 +1,10 @@
 import math
-from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver import Remote
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ExpCond
 
 
 class BasePage:
@@ -33,6 +36,25 @@ class BasePage:
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
+            return False
+
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                ExpCond.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1,
+                          TimeoutException).until_not(
+                              ExpCond.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
 
         return True
