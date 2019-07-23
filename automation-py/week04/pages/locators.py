2044dbeb58d319caa2b3102247e2f1387a8b65fb
Vladimir Petrigo <taenaru@gmail.com>
2019-07-23 21:50:49 +0300

    automation: Refactor locators - move Main to BasePage

--- automation-py/week04/pages/locators.py
+++ automation-py/week04/pages/locators.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    REGISTRATION_LINK = (By.CSS_SELECTOR, "#login_link")
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inv")
 
 
 class LoginPageLocators:
