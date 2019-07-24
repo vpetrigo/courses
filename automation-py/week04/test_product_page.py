461e6db24dd9f2477bc0896d90c564551188a359
Vladimir Petrigo <taenaru@gmail.com>
2019-07-24 23:51:45 +0300

    automation: Add new tests

--- automation-py/week04/test_product_page.py
+++ automation-py/week04/test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 links = [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
@@ -44,3 +45,20 @@ def test_guest_cant_see_success_message(browser) -> None:
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_not_see_success_message_upon_opening_product_page()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser) -> None:
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
