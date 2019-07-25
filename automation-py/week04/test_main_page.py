95626e51e97a7e641f407bb0e5fecb942fff078b
Vladimir Petrigo <taenaru@gmail.com>
2019-07-26 00:42:07 +0300

    automation: Clean up

--- automation-py/week04/test_main_page.py
+++ automation-py/week04/test_main_page.py
@@ -1,12 +1,27 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.cart_page import CartPage
+
+import pytest
+
+main_page_link = "http://selenium1py.pythonanywhere.com"
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_login_page()
-    main_page.should_be_login_link()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    # не забываем передать первым аргументом self
+    def test_guest_can_go_to_login_page(self, browser) -> None:
+        main_page = MainPage(browser, main_page_link)
+        main_page.open()
+        main_page.go_to_login_page()
+        main_page.should_be_login_link()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser) -> None:
+        main_page = MainPage(browser, main_page_link)
+        main_page.open()
+        main_page.go_to_cart_page()
+        cart_page = CartPage(browser, browser.current_url)
+        cart_page.should_be_empty()
+        cart_page.should_contain_empty_text()
--- automation-py/week04/test_product_page.py
+++ automation-py/week04/test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.cart_page import CartPage
 
 links = [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
@@ -10,6 +11,8 @@ links = [
 
 offer_link_template = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
 offer_links = [f"{offer_link_template}{i}" for i in range(0, 10)]
+
+product_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
 
 
 @pytest.mark.parametrize("link", links)
@@ -21,6 +24,7 @@ def test_guest_can_add_product_to_cart(browser, link: str) -> None:
     product_page.should_check_overall_cost()
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("link", offer_links)
 def test_guest_can_add_product_to_cart_with_different_offer_numbers(
         browser, link: str) -> None:
@@ -32,8 +36,7 @@ def test_guest_can_add_product_to_cart_with_different_offer_numbers(
 
 
 def test_guest_can_add_non_promo_product_to_cart(browser) -> None:
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-    product_page = ProductPage(browser, link)
+    product_page = ProductPage(browser, product_link)
     product_page.open()
     product_page.add_to_cart()
     product_page.should_be_present_in_cart()
@@ -41,24 +44,31 @@ def test_guest_can_add_non_promo_product_to_cart(browser) -> None:
 
 
 def test_guest_cant_see_success_message(browser) -> None:
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-    product_page = ProductPage(browser, link)
+    product_page = ProductPage(browser, product_link)
     product_page.open()
     product_page.should_not_see_success_message_upon_opening_product_page()
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+def test_guest_should_see_login_link_on_product_page(browser) -> None:
+    page = ProductPage(browser, product_link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser) -> None:
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, product_link)
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser
+                                                                 ) -> None:
+    page = ProductPage(browser, product_link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_be_empty()
+    cart_page.should_contain_empty_text()
