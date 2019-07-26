571921c4e7d670ae0736b878a8cb970576933bf3
Vladimir Petrigo <taenaru@gmail.com>
2019-07-26 23:27:52 +0300

    automation: Update locators with registration form inputs

--- automation-py/week04/pages/locators.py
+++ automation-py/week04/pages/locators.py
@@ -2,12 +2,18 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inv")
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_LINK_INVALID = (By.ID, "login_link_inv")
     CART_BUTTON = (By.CSS_SELECTOR, ".basket-mini a.btn.btn-default")
+    USER_ICON = (By.CLASS_NAME, "icon-user")
 
 
 class LoginPageLocators:
+    LOGIN_BUTTON = (By.ID, "login_link")
+    EMAIL_INPUT = (By.NAME, "registration-email")
+    PASSWORD_INPUT = (By.NAME, "registration-password1")
+    REPEAT_PASSWORD_INPUT = (By.NAME, "registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
 
